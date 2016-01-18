43d1a7b6996caf7f6e7f031ec10694f3a426e98d
lpatmo <linda.peng@alumni.duke.edu>
2016-01-17 16:34:39 -0800

    started adding user statuses; not quite finished yet. thanks to @anbuselvan for the meteor guidance.

--- cb-v2-scratch.js
+++ cb-v2-scratch.js
@@ -11,6 +11,16 @@ if (Meteor.isClient) {
     return Meteor.users.find({ "status.online": true }).count();
     }
   });
+
+  Template.activeUsers.events({
+    'focusout textarea#current_status': function(event) {
+        //console.log(event.target.currentStatus.value);
+      var currentStatusVar = $(event.target).val();
+      console.log(currentStatusVar);
+      Meteor.call(setUserStatus, currentStatusVar);
+
+    }
+  });
   
   Template.activeUsers.labelClass = function() {
   if (this.status.idle)
@@ -25,9 +35,28 @@ if (Meteor.isClient) {
    Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
    });
+   
+   Meteor.methods({
+    setUserStatus: function(currentStatusVar) {
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+        var currentUserId = Meteor.userId();
+        Users.insert({
+            currentStatusVar: currentStatusVar,
+            createdBy: currentUserId
+        });
+    }
+  });
 }
 
 if (Meteor.isServer) {
+  Accounts.onCreateUser(function(user) {
+    user.setUserStatus = "";
+    return user;
+  });
+
+  
 
   Meteor.publish("userStatus", function() {
     return Meteor.users.find({ "status.online": true });
