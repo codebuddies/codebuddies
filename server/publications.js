59f776911e395401eed935903d1ffc5fc646205c
lpatmo <linda.peng@alumni.duke.edu>
2016-03-13 18:19:41 -0400

    properly return list of learnings submitted by user on profile page

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,10 @@ Meteor.publish("learnings", function(limit) {
   return Learnings.find({}, {limit: limit, createdAt: 1});
 });
 
+Meteor.publish("ownLearnings", function(limit) {
+  return Learnings.find({owner: this.userId}, {limit: limit, createdAt: 1});
+});
+
 Meteor.publish("hangouts", function() {
   return Hangouts.find();
 });
