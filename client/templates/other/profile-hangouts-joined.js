fda475f5b3056de3245178ce660c1d13f1d43023
distalx <distalx@gmail.com>
2016-03-17 20:11:59 +0530

    pagination on scroll implimented for user profile

--- client/templates/other/profile-hangouts-joined.js
+++ client/templates/other/profile-hangouts-joined.js
@@ -1,59 +1,52 @@
+Template.hangoutsJoined.onRendered(function() {
+   var instance = this;
+   $('#hangout-joined-pagination').bind('scroll', function(){
+       if($('#hangout-joined-pagination').scrollTop() + $('#hangout-joined-pagination').innerHeight()>=$('#hangout-joined-pagination')[0].scrollHeight){
+          console.log("message");
+
+           if(Hangouts.find().count() === instance.limit.get()){
+             instance.limit.set(instance.limit.get() + 5);
+           }else {
+             if(Hangouts.find().count() < instance.limit.get()){
+               instance.flag.set(true);
+             }
+           }
+       }
+   });
+});
 Template.hangoutsJoined.onCreated(function () {
 
-  // 1. Initialization
-
   var instance = this;
+  instance.limit = new ReactiveVar(4);
+  instance.flag = new ReactiveVar(false);
 
-  // initialize the reactive variables
-  instance.loaded = new ReactiveVar(0);
-  instance.limit = new ReactiveVar(3);
+  instance.autorun(function () {
 
-  // ...
-   instance.autorun(function () {
-
-    // get the limit
     var limit = instance.limit.get();
 
-    //console.log("Asking for "+limit+" hangouts...")
-
-    // subscribe to the posts publication
     var subscription = instance.subscribe('hangoutsJoined', limit);
-
-    // if subscription is ready, set limit to newLimit
-    if (subscription.ready()) {
-      //console.log("> Received "+limit+" hangouts. \n\n")
-      instance.loaded.set(limit);
-    } else {
-      //console.log("> Subscription is not ready yet. \n\n");
-    }
   });
 
-   instance.hangoutsJoined = function() { 
-    return Hangouts.find({}, {sort: {timestamp: -1}, limit: instance.loaded.get()});
+   instance.hangoutsJoined = function() {
+    return Hangouts.find({}, {sort: {timestamp: -1}});
   }
 
 });
 
 Template.hangoutsJoined.helpers({
-    // the posts cursor
+
   hangouts: function () {
     return Template.instance().hangoutsJoined();
   },
-  // are there more posts to show?
+
   hasMoreHangouts: function () {
-    return Template.instance().hangoutsJoined().count() >= Template.instance().limit.get();
+    return Template.instance().flag.get();
   }
 });
 
 Template.hangoutsJoined.events({
- 'click #load-more-hangouts': function (event, instance) {
-    event.preventDefault();
-
-    // get current value for limit, i.e. how many posts are currently displayed
-    var limit = instance.limit.get();
-
-    // increase limit by 5 and update it
-    limit += 3;
-    instance.limit.set(limit);
+  "click #go-to-top": function(event, template){
+    var pos = document.getElementById("hangout-joined-pagination");
+    pos.scrollTop = - pos.scrollHeight;
   }
 });
