e20eb20c79e24cba03140847c964f54abf9581bb
lpatmo <linda.peng@alumni.duke.edu>
2016-05-26 01:19:18 -0400

    forget this header

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ Picker.middleware(bodyParser.json());
 Picker.route('/hangout/:_id', function(params, req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	//add in some checking ot make sure re.body.hangoutUrl is a hangout URL	
   Hangouts.update(params._id, { $set: { url: req.body.hangoutUrl }}, function(err, result){ 
   	if (err) {
