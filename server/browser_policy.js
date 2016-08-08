47794eab9b4bef68650f6bf159aa67fdafb23451
distalx <distalx@gmail.com>
2016-08-08 06:03:16 +0530

    added : browser policies

---
+++ server/browser_policy.js
@@ -0,0 +1,23 @@
+BrowserPolicy.framing.disallow();
+BrowserPolicy.content.disallowInlineScripts();
+BrowserPolicy.content.disallowEval();
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowFontDataUrl();
+BrowserPolicy.content.allowInlineScripts();
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowOriginForAll('http://*.materialdesignicons.com');
+var trusted = [
+  '*.herokuapp.com',
+  '*.gstatic.com',
+  '*.materialdesignicons.com',
+  '*.googleapis.com',
+  '*.google.com',
+  '*.gravatar.com',
+  '*.slack.com',
+  '*.wp.com'
+];
+
+_.each(trusted, function(origin) {
+  origin = "https://" + origin;
+  BrowserPolicy.content.allowOriginForAll(origin);
+});
