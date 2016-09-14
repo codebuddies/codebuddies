02da6e5b7883bad2ec4bd3d9e868668529fcc626
distalx <distalx@gmail.com>
2016-09-14 18:12:34 +0530

    added: pagination to hangout-join list

--- server/hangouts/publications.js
+++ server/hangouts/publications.js
@@ -1,4 +1,6 @@
 Meteor.publish("hangouts", function(limit) {
+  check(limit, Number);
+
   if(this.userId) {
     if (Roles.userIsInRole(this.userId, ['admin','moderator'])) {
       return Hangouts.find({}, {fields:{'email_addresses': 0 }, 'limit':limit});
@@ -11,37 +13,39 @@ Meteor.publish("hangouts", function(limit) {
 
 });
 
-// Meteor.publish("hangoutById", function(hangoutId) {
-//
-//   if (Roles.userIsInRole(this.userId, ['admin','moderator'])) {
-//
-//     return Hangouts.find({_id: hangoutId},
-//                          {fields:{'email_addresses': 0 }});
-//   } else {
-//
-//     return Hangouts.find({_id: hangoutId, 'visibility':{ $ne: false} },
-//                          {fields: {'email_addresses': 0 }} );
-//   }
-//
-// });
+Meteor.publish("hangoutById", function(hangoutId) {
+
+  if (Roles.userIsInRole(this.userId, ['admin','moderator'])) {
+
+    return Hangouts.find({_id: hangoutId},
+                         {fields:{'email_addresses': 0 }});
+  } else {
+
+    return Hangouts.find({_id: hangoutId, 'visibility':{ $ne: false} },
+                         {fields: {'email_addresses': 0 }} );
+  }
+
+});
 
 
-// Meteor.publish("hangoutsJoined", function(limit) {
-//
-//   if(this.userId) {
-//     return Hangouts.find({users:{ $elemMatch:{ $eq: this.userId}},'visibility':{ $ne: false}},
-//                          {sort: {timestamp: -1}, limit: limit});
-//   } else {
-//    this.ready();
-//   }
-//
-// });
-//
-// Meteor.publish("hangoutSearchResult", function(serchTerm) {
-//
-//   check(serchTerm, String);
-//   if (_.isEmpty(serchTerm)){
-//     this.ready();
-//   }
-//   return Hangouts.search(serchTerm);
-// });
+Meteor.publish("hangoutsJoined", function(limit, userId) {
+  check(limit, Number);
+  check(userId, String);
+
+  if(this.userId) {
+    return Hangouts.find({users:{ $elemMatch:{ $eq: userId}},'visibility':{ $ne: false}},
+                         {sort: {timestamp: -1}, limit: limit});
+  } else {
+   this.ready();
+  }
+
+});
+
+Meteor.publish("hangoutSearchResult", function(serchTerm) {
+
+  check(serchTerm, String);
+  if (_.isEmpty(serchTerm)){
+    this.ready();
+  }
+  return Hangouts.search(serchTerm);
+});
