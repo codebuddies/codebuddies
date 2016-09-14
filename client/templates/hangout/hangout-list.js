329388a06131a8d8292ba69ba3490ed06dbe5132
distalx <distalx@gmail.com>
2016-09-14 16:55:01 +0530

    added: pagination on hangout list

--- client/templates/hangout/hangout-list.js
+++ client/templates/hangout/hangout-list.js
@@ -5,13 +5,8 @@ Template.hangoutList.onCreated(function() {
 
    instance.autorun(function () {
      var limit = instance.limit.get();
-     var subscription = instance.subscribe('hangouts', limit);
+     instance.subscribe('hangouts', limit);
    });
-
-   instance.loadHangouts = function() {
-    //  console.log('hangout count inside template', Hangouts.find().count());
-     return Hangouts.find();
-   }
 
 });
 
@@ -19,16 +14,23 @@ Template.hangoutList.onCreated(function() {
 Template.hangoutList.onRendered(function() {
     var instance = this;
 
-    instance.loadmoreHangouts = function(){
-      if(Hangouts.find().count() === instance.limit.get()){
-        instance.limit.set(instance.limit.get() + 5);
-      }else {
-        if(Hangouts.find().count() < instance.limit.get()){
-          instance.flag.set(true);
-        }
-      }
+    instance.loadHangouts = function() {
+
+      return Hangouts.find();
     }
 
+    instance.addMoreHangouts = function(){
+        console.log("loadmore called");
+
+        if(Hangouts.find().count() === instance.limit.get()){
+             instance.limit.set(instance.limit.get() + 5);
+
+        }else{
+           if(Hangouts.find().count() < instance.limit.get()){
+               instance.flag.set(true);
+           }
+       }
+    }
 
 });
 
@@ -43,80 +45,6 @@ Template.hangoutList.helpers({
 
 Template.hangoutList.events({
   "click #loadMore": function(event, template){
-    template.instance().loadmoreHangouts();
+     template.addMoreHangouts();
   }
 });
-
-
-
-
-// Template.hangoutList.onCreated(function() {
-//   var self = this;
-//   self.pageSize = 5;
-//   self.currentCount = new ReactiveVar(this.pageSize);
-//   self.loadMore = new ReactiveVar(false);
-//   self.hangoutsCount = 0;
-//   self.loadMoreText = new ReactiveVar(TAPi18n.__("load_more"));
-//   Meteor.call('getHangoutsCount', Meteor.userId(), function(err, result) {
-//     self.hangoutsCount = result.hangoutsCount;
-//     if (self.hangoutsCount > self.pageSize) {
-//       self.loadMore.set(true);
-//     }
-//     self.currentCount.set(self.pageSize);
-//     var totalSizeStored = Session.set('totalSizeStored', self.hangoutsCount);
-//     var pageSize = self.pageSize;
-//     var totalSize = self.hangoutsCount;
-//     var currentSize = self.currentCount.get();
-//     var currentPage = (parseInt(currentSize / pageSize) + 1);
-//     var totalPages = parseInt(Math.ceil(totalSize / pageSize));
-//     self.loadMoreText.set(TAPi18n.__("load_page", {start: currentPage, total: totalPages}));
-//   });
-//   self.ready = new ReactiveVar(false);
-// });
-//
-// Template.hangoutList.helpers({
-//   isReady: function() {
-//     var template = Template.instance();
-//     template.ready.set(FlowRouter.subsReady());
-//     return template.ready.get();
-//   },
-//   hangouts: function () {
-//     var template = Template.instance();
-//     if (!template.ready.get()) return null;
-//     var size = template.currentCount.get();
-//     return Hangouts.find({},{ sort: { start: -1 }, limit: size});
-//   },
-//   showLoadMore: function() {
-//     return Template.instance().loadMore.get();
-//   },
-//   loadMoreText: function() {
-//     var template = Template.instance();
-//     var pageSize = template.pageSize;
-//     var currentSize = template.currentCount.get();
-//     if (template.hangoutsCount > 0) {
-//       var totalSize = template.hangoutsCount;
-//       var currentPage = (parseInt(currentSize / pageSize) + 1);
-//       var totalPages = parseInt(Math.ceil(totalSize / pageSize));
-//       template.loadMoreText.set(TAPi18n.__("load_page", {start: currentPage, total: totalPages}));
-//     }
-//     return template.loadMoreText.get();
-//   }
-// });
-//
-// Template.hangoutList.events({
-//   'click #btn-load-more': function() {
-//     var template = Template.instance();
-//     var pageSize = template.pageSize;
-//     var totalSize = Session.get('totalSizeStored');
-//     var currentSize = template.currentCount.get();
-//     var newSize = currentSize;
-//
-//     if ((newSize + pageSize) < totalSize)
-//       newSize += pageSize;
-//     else {
-//       newSize += (totalSize - newSize);
-//       template.loadMore.set(false);
-//     }
-//     template.currentCount.set(newSize);
-//   }
-// });
--- server/hangouts/publications.js
+++ server/hangouts/publications.js
@@ -1,27 +1,15 @@
-Meteor.publish("hangouts", function(limit){
-  console.log(limit);
-  console.log("pub hangouts", Hangouts.find({},{limit:limit}).count());
-  return Hangouts.find({},{limit:limit});
+Meteor.publish("hangouts", function(limit) {
+  if(this.userId) {
+    if (Roles.userIsInRole(this.userId, ['admin','moderator'])) {
+      return Hangouts.find({}, {fields:{'email_addresses': 0 }, 'limit':limit});
+    } else {
+      return Hangouts.find({'visibility':{$ne:false}}, {fields:{'email_addresses': 0 }, 'limit':limit});
+    }
+  } else {
+   this.ready();
+  }
 
 });
-
-// Meteor.publish("hangouts", function(limit) {
-//   check(limit, Number);
-//   console.log(limit);
-//   if(this.userId) {
-//     if (Roles.userIsInRole(this.userId, ['admin','moderator'])) {
-//       console.log(" pub hangouts", Hangouts.find({}, {fields:{'email_addresses': 0 }}, {limit:limit} ).count());
-//       return Hangouts.find({}, {fields:{'email_addresses': 0 }}, {limit:limit} );
-//     } else {
-//       console.log(" pub hangouts", Hangouts.find({}, {fields:{'email_addresses': 0 }}, {limit:limit} ).count());
-//       return Hangouts.find({'visibility':{$ne:false}}, {fields:{'email_addresses': 0 }}, {limit:limit});
-//     }
-//   } else {
-//    this.ready();
-//   }
-//
-//
-// });
 
 // Meteor.publish("hangoutById", function(hangoutId) {
 //
