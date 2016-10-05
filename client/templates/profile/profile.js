2552dfacea8f36c133cc899f8d59d82358c56a64
Sharynne Azhar <sharynneazhar@gmail.com>
2016-10-05 00:14:52 -0500

    add first iteration of edit fields

---
+++ client/templates/profile/profile.js
@@ -0,0 +1,30 @@
+Template.profile.onCreated(function(){
+  var username = FlowRouter.getParam('name');
+  var title = username + " | Profile";
+  DocHead.setTitle(title);
+});
+
+Template.registerHelper('editMode',function(){
+    return Session.get("editMode");
+});
+
+Template.profile.helpers({
+  userInfo: function() {
+		var userId = FlowRouter.getParam('userId');
+    console.log(ReactiveMethod.call('getUserDetails',userId));
+		return ReactiveMethod.call('getUserDetails',userId);
+  },
+  hangoutsJoinedCount: function() {
+		var userId = FlowRouter.getParam('userId');
+  	return ReactiveMethod.call('getHangoutsJoinedCount',userId)
+  }
+});
+
+Template.profile.events({
+  'click .editProfile': function() {
+    Session.set('editMode',true);
+  },
+  'click #cancelProfileEdit': function() {
+    Session.set('editMode',false);
+  },
+});
