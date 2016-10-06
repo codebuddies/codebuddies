5959e5c5d740364851bc9046895a55224c87b997
distalx <distalx@gmail.com>
2016-10-06 09:55:43 +0530

    added : popup for asking visitors to sign in.

--- client/templates/hangout/hangout_card.js
+++ client/templates/hangout/hangout_card.js
@@ -3,3 +3,14 @@ Template.hangoutCard.helpers({
     return topic.truncate()
   }
 });
+
+Template.hangoutCard.events({
+  "click #visitor": function(event, template){
+    event.preventDefault();
+    sweetAlert({
+      title: TAPi18n.__("sign_in_to_continue"),
+      confirmButtonText: TAPi18n.__("ok"),
+      type: 'info'
+    });
+  }
+});
