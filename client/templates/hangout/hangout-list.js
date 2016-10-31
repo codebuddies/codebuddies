ed86fdc38e565bf05f4a943d517deeec0e90cd2a
Luke Camilleri <mail@camilleriluke.com>
2016-10-31 18:10:38 +0100

    Runtime error fix.
    Helper functions are now being added on creation of the template instead on render.
    
    This was cauing `Exception in template helper: TypeError: Template.instance(...).loadHangouts is not a function` to occur.

--- client/templates/hangout/hangout-list.js
+++ client/templates/hangout/hangout-list.js
@@ -1,49 +1,44 @@
-Template.hangoutList.onCreated(function() {
-  var instance = this;
-   instance.limit = new ReactiveVar(5);
-   instance.flag = new ReactiveVar(false);
-
-   instance.autorun(function () {
-     var limit = instance.limit.get();
-     instance.subscribe('hangouts', limit);
-   });
-
-});
-
-
-Template.hangoutList.onRendered(function() {
+Template.hangoutList.onCreated(function () {
     var instance = this;
+    instance.limit = new ReactiveVar(5);
+    instance.flag = new ReactiveVar(false);
 
-    instance.loadHangouts = function() {
+    instance.autorun(function () {
+        var limit = instance.limit.get();
+        instance.subscribe('hangouts', limit);
+    });
 
-      return Hangouts.find({}, {sort: {start: -1}});
+    instance.loadHangouts = function () {
+
+        return Hangouts.find({}, { sort: { start: -1 } });
     }
 
-    instance.addMoreHangouts = function(){
+    instance.addMoreHangouts = function () {
 
-        if(Hangouts.find().count() === instance.limit.get()){
-             instance.limit.set(instance.limit.get() + 8);
+        if (Hangouts.find().count() === instance.limit.get()) {
+            instance.limit.set(instance.limit.get() + 8);
 
-        }else{
-           if(Hangouts.find().count() < instance.limit.get()){
-               instance.flag.set(true);
-           }
-       }
+        } else {
+            if (Hangouts.find().count() < instance.limit.get()) {
+                instance.flag.set(true);
+            }
+        }
     }
-
 });
+
+
 
 Template.hangoutList.helpers({
-  hangouts:function(){
-    return Template.instance().loadHangouts();
-  },
-  status:function(){
-     return  Template.instance().flag.get();
-  }
+    hangouts: function () {
+        return Template.instance().loadHangouts();
+    },
+    status: function () {
+        return Template.instance().flag.get();
+    }
 });
 
 Template.hangoutList.events({
-  "click #loadMore": function(event, template){
-     template.addMoreHangouts();
-  }
+    "click #loadMore": function (event, template) {
+        template.addMoreHangouts();
+    }
 });
