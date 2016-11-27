3fcecc6590c0ab5d4a796cb20bfe091953b1a236
sergeant-q <sgtquezada@gmail.com>
2016-11-27 13:18:53 -0800

    Fixed additional lodash related issues (#419)
    * fix for issue 300 - working on lodash update
    
    * reverted back to meteor lodash package for quilljs compatability

--- client/layouts/layout.js
+++ client/layouts/layout.js
@@ -1,8 +1,9 @@
 Template.layout.events({
   "click .continue-popup": function(event, template){
+    event.preventDefault();
     if (!Meteor.userId()) {
       sweetAlert({
-        text:  TAPi18n.__("continue_popup_text") ,
+        html: '<b>' + TAPi18n.__("continue_popup_text") + '</b>',
         showCancelButton: true,
         confirmButtonText: TAPi18n.__("continue_with_slack"),
         cancelButtonText: TAPi18n.__("not_now"),
--- client/templates/hangout/hangout-item.js
+++ client/templates/hangout/hangout-item.js
@@ -20,7 +20,8 @@ Template.hangoutItem.helpers({
 });
 
 Template.hangoutItem.events({
-  'click #join-hangout': function() {
+  'click .join-hangout': function() {
+    console.log('clicked on join hangout')
     if (!Meteor.userId()) {
       sweetAlert({
         title: TAPi18n.__("you_are_almost_there"),
