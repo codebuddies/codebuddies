688a5714bb32fb183fdc1d1812359e8c096f224e
lpatmo <linda.peng@alumni.duke.edu>
2017-02-10 01:22:03 -0800

    improvements for the hangout post page - added jitsi screenshot, added 'create a hangout' ask after hangout is over, and improved styles

--- client/templates/hangout/hangout.js
+++ client/templates/hangout/hangout.js
@@ -112,5 +112,23 @@ Template.hangout.events({
 
       }); //sweetAlert
 
+  },
+  "click #create-hangout-popup": function() {
+    if (!Meteor.userId()) {
+      sweetAlert({
+        title: TAPi18n.__("login_create_hangout_title"),
+        text: TAPi18n.__("login_create_hangout_message"),
+        confirmButtonText: TAPi18n.__("sign_in_with_slack"),
+        type: 'info'
+      },
+      function(){
+        var options = {
+          requestPermissions: ['identify', 'users:read']
+        };
+        Meteor.loginWithSlack(options);
+      });
+    } else {
+      Modal.show('createHangoutModal');
+    }
   }
 });
