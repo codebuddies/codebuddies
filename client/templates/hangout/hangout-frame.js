1f1f70c49359e45139966d850337a7c9e124214f
lpatmo <linda.peng@alumni.duke.edu>
2017-02-11 21:08:21 -0800

    load user avatar as well

--- client/templates/hangout/hangout-frame.js
+++ client/templates/hangout/hangout-frame.js
@@ -12,14 +12,14 @@ Template.hangoutFrame.onCreated(function() {
     let htmlElement = undefined;
 
     instance.api = new JitsiMeetExternalAPI(domain, room, width, height, htmlElement, configOverwrite, interfaceConfigOverwrite);
+    instance.api.executeCommand('displayName', data.username);
+    instance.api.executeCommand('toggleVideo');
+    instance.api.executeCommand('toggleChat')
+    instance.api.executeCommand('avatarUrl', data.avatar)
 
     $('#jitsiConference0').appendTo('div#hangout-container').css('width','100%');
     //only show the launch hangout button if Jitsi is not loaded
     $('#jitsiConference0').length == 1 ? $('.load-hangout').hide() : $('#load-hangout').show();
-
-    instance.api.executeCommand('displayName', data.username);
-    instance.api.executeCommand('toggleVideo');
-
   }
 
   instance.disposeJitsi= function(){
@@ -30,11 +30,11 @@ Template.hangoutFrame.onCreated(function() {
 
 Template.hangoutFrame.events({
   'click .load-hangout': function(event, template){
-
     const data = {
       room: this._id,
       username: Meteor.user().username,
-      type: this.type
+      type: this.type,
+      avatar: Meteor.user().profile.avatar.default
     }
     return template.loadJitsi(data);
   }
