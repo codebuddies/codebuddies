e06f8cfb7120a9e4c7a0c082c827b7468039755b
lpatmo <linda.peng@alumni.duke.edu>
2017-03-16 01:51:55 -0700

    add the guts for the 'what did you learn?' box

---
+++ client/templates/hangout/hangout-learned.js
@@ -0,0 +1,51 @@
+Template.registerHelper("learningOwner", function(ownerid){
+  if(Meteor.userId() === ownerid){
+      return true;
+  }else {
+      return false;
+  }
+
+});
+
+Template.hangoutLearned.events({
+	 'keypress textarea#learned-text': function(event) {
+	 	if (event.which === 13) {
+		        if (!Meteor.userId()) {
+		            sweetAlert({
+		                title: TAPi18n.__("you_are_almost_there"),
+		                text: TAPi18n.__("login_update_status"),
+		                confirmButtonText: TAPi18n.__("sign_in_with_slack"),
+		                type: 'info'
+		            },
+		            function(){
+		              var options = {
+		                requestPermissions: ['identify', 'users:read']
+		              };
+		              Meteor.loginWithSlack(options);
+		            });
+		        } else {
+		            var learningStatus = $('#learned-text').val();
+
+		            if ($.trim(learningStatus) == '') {
+		                $('#topic').focus();
+		                sweetAlert({
+		                    title: TAPi18n.__("Please share something you've learned"),
+		                    confirmButtonText: TAPi18n.__("ok"),
+		                    type: 'error'
+		                });
+		                return;
+		            }
+		            var data = {
+		                user_id: Meteor.userId(),
+		                username: Meteor.user().username,
+		                title: learningStatus,
+		                hangout_id: this._id
+		            }
+		            Meteor.call("addLearning", data, function(error, result) {});
+		            $('#learned-text').val('What did you learn?');
+					$('.learnedCharactersLeft').text(140);
+		        }
+		    }
+    },
+
+});
