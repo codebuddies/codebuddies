173bab8fc7d9c3a80e857495c93955fd15040a4f
Linda <linda.peng@alumni.duke.edu>
2017-03-22 02:02:30 -0700

    Add "Learnings" section to each single post (#478)
    * add the guts for the 'what did you learn?' box
    
    * new fiel
    
    * unfinished: show learnings associated with hangout post
    
    * fix problem: learnings attached to specific hangouts were not showing up on posts
    
    * make language clearer that user has to sign in to the codebuddies.slack.com account
    
    * Make sure learnings appear publicly even for those who are logged out
    
    * improve popup screen for non-logged-in users (see issue #477)
    
    * improve UI for learnings on post
    
    * align left side of page
    
    * add 140 character check for learning and link h3 tag to 'see all learnings' link
    
    * Allow participants to continue to be in the hangout after scheduled time ends (#463)
    
    * Fixes logo issue #428 in CB hangout reminder email (#434)
    
    * Fixes logo issue #428 in CB hangout reminder email
    
    * Changed img src to {{logo}} fixing logo issue #428
    
    * Changed logo image from logo.svg to logo-circle.png
    
    * fix for issue 462: continue to allow participants to be in the hangout after the hangout has ended
    
    * Issue 470 - fix for favicon not appearing (#472)
    
    * Fixes logo issue #428 in CB hangout reminder email (#434)
    
    * Fixes logo issue #428 in CB hangout reminder email
    
    * Changed img src to {{logo}} fixing logo issue #428
    
    * Changed logo image from logo.svg to logo-circle.png
    
    * fix for issue 470: new favicon
    
    * changed "Crea" to "Programa"
    
    * Changed "Create" to "Schedule"
    
    Fixes Issue #469.
    
    * Update en.i18n.json
    
    Changed all instances of "hangout" to "Hangout" per issue #475
    
    * Update es.i18n.json
    
    Fixes #475 
    Changed "(h)angout" to "(H)angout"
    
    * add the guts for the 'what did you learn?' box
    
    * new fiel
    
    * unfinished: show learnings associated with hangout post
    
    * fix problem: learnings attached to specific hangouts were not showing up on posts
    
    * make language clearer that user has to sign in to the codebuddies.slack.com account
    
    * Make sure learnings appear publicly even for those who are logged out
    
    * improve popup screen for non-logged-in users (see issue #477)
    
    * improve UI for learnings on post
    
    * align left side of page
    
    * add 140 character check for learning and link h3 tag to 'see all learnings' link
    
    * switch default to upcoming instead of live hangouts on non-logged-in hangouts page

--- client/layouts/layout.js
+++ client/layouts/layout.js
@@ -3,7 +3,10 @@ Template.layout.events({
     event.preventDefault();
     if (!Meteor.userId()) {
       sweetAlert({
-        html: '<b>' + TAPi18n.__("continue_popup_text") + '</b>',
+        imageUrl: '/images/slack-signin-example.jpg',
+        imageSize: '140x120',
+        title: TAPi18n.__("you_are_almost_there"),
+        html: TAPi18n.__("continue_popup_text"),
         showCancelButton: true,
         confirmButtonText: TAPi18n.__("continue_with_slack"),
         cancelButtonText: TAPi18n.__("not_now"),
