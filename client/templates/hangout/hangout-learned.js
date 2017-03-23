42d37bbb90cdd9367ba553407ab82d47c1d668d8
distalx <distalx@gmail.com>
2017-03-23 09:38:51 +0530

    remove repetitive code

--- client/templates/hangout/hangout-learned.js
+++ client/templates/hangout/hangout-learned.js
@@ -18,58 +18,38 @@ Template.hangoutLearned.events({
 	},
   'keypress textarea#learned-text': function(event) {
 	 	if (event.which === 13) {
-		        if (!Meteor.userId()) {
-		            sweetAlert({
-		            	imageUrl: '/images/slack-signin-example.jpg',
-        				imageSize: '140x120',
-        				showCancelButton: true,
-		                title: TAPi18n.__("you_are_almost_there"),
-		                html: TAPi18n.__("continue_popup_text"),
-		                confirmButtonText: TAPi18n.__("sign_in_with_slack"),
-		                cancelButtonText: TAPi18n.__("not_now")
-		            },
-		            function(){
-		              var options = {
-		                requestPermissions: ['identify', 'users:read']
-		              };
-		              Meteor.loginWithSlack(options);
-		            });
-		        } else {
-		            var learningStatus = $('#learned-text').val();
+      var learningStatus = $('#learned-text').val();
 
-		            if ($.trim(learningStatus) == '') {
-		                $('#topic').focus();
-		                sweetAlert({
-		                    title: TAPi18n.__("Please share something you've learned"),
-		                    confirmButtonText: TAPi18n.__("ok"),
-		                    type: 'error'
-		                });
-		                return;
-		            }
-		            var data = {
-		                user_id: Meteor.userId(),
-		                username: Meteor.user().username,
-		                title: learningStatus,
-		                hangout_id: this._id
-		            }
-		            Meteor.call("addLearning", data, function(error, result) {
-                  if (error) {
-                    console.log(error);
-                  }
-                  if (result) {
-                    swal({
-                       type: "success",
-                       text: "Thank you for sharing what you learned!",
-                       timer: 500,
-                       showConfirmButton: false
-                     });
-                     $('#learned-text').val('').blur();
-                     $('.learnedCharactersLeft').text(140);
-                  }
-                });
-
-		        }
-		    }
-      },
-
+      if ($.trim(learningStatus) == '') {
+        $('#topic').focus();
+        sweetAlert({
+          title: TAPi18n.__("Please share something you've learned"),
+          confirmButtonText: TAPi18n.__("ok"),
+          type: 'error'
+        });
+        return;
+      }
+      var data = {
+        user_id: Meteor.userId(),
+        username: Meteor.user().username,
+        title: learningStatus,
+        hangout_id: this._id
+      }
+      Meteor.call("addLearning", data, function(error, result) {
+        if (error) {
+          console.log(error);
+        }
+        if (result) {
+          swal({
+             type: "success",
+             text: "Thank you for sharing what you learned!",
+             timer: 500,
+             showConfirmButton: false
+           });
+           $('#learned-text').val('').blur();
+           $('.learnedCharactersLeft').text(140);
+        }
+      });
+    }
+  }
 });
