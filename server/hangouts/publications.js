a14ccc6acca584b3d8c97c95b5655424a36dc2a7
distalx <distalx@gmail.com>
2017-07-04 09:13:57 +0530

    fix hangouts publication

--- server/hangouts/publications.js
+++ server/hangouts/publications.js
@@ -3,14 +3,15 @@ Meteor.publish("hangouts", function(limit) {
 
 
   if(this.userId) {
-    console.log("here");
+
     if (Roles.userIsInRole(this.userId, ['admin','moderator'], 'CB')) {
       return Hangouts.find({}, {fields:{'email_addresses': 0 }, sort: { start: -1 }, 'limit':limit});
     } else {
       return Hangouts.find({'visibility':{$ne:false}}, {fields:{'email_addresses': 0 }, sort: { start: -1 }, 'limit':limit});
     }
+  }
 
-
+  this.ready();
 });
 
 Meteor.publish("hangoutById", function(hangoutId) {
