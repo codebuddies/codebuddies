e3bddf962f0dfc87f87cc43e1400e05232ad09e5
distalx <distalx@gmail.com>
2017-07-04 09:34:26 +0530

    modify role update to accommodate groups

--- client/templates/admin/user/user_by_id.js
+++ client/templates/admin/user/user_by_id.js
@@ -18,18 +18,24 @@ Template.userById.helpers({
       return Meteor.users.find({_id:userId});
     }
 
-
-
+  },
+  userRoles: function(){
+    const roles = Meteor.users.findOne({_id: FlowRouter.getParam('userId') }).roles;
+    const roleGroup = 'CB';
+    return roles[roleGroup] ;
   },
 });
 
 Template.userById.events({
   "change #authorization": function(event, template){
     var currentAuthorization = template.find('#authorization').value;
-    var pastAuthorization =  this.roles[0];
+    console.log(this);
+    const roleGroup = 'CB';
+    var pastAuthorization =  this.roles[roleGroup].pop();
+    console.log(pastAuthorization, currentAuthorization);
     var userId = this._id;
     var username = this.username;
-    Meteor.call("updateRoles",this._id,this.username,currentAuthorization,pastAuthorization, function(error, result) {
+    Meteor.call("updateRoles",this._id,this.username,currentAuthorization,pastAuthorization, roleGroup, function(error, result) {
       if (result) {
         swal("Done!", "Updated "+ username +"'s access right ", "success")
       }else{
