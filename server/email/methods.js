f00071d952bf5eeaa2781ceb10136cc054896f8b
lpatmo <linda.peng@alumni.duke.edu>
2017-08-26 13:47:19 -0700

    updates for email notifications - add a followup email notification as well

--- server/email/methods.js
+++ server/email/methods.js
@@ -1,13 +1,22 @@
 emailNotification = function(hangout, type){
   let emails = hangout.email_addresses.join(",");
+  console.log(emails + ' are the emails that we are sending to in emailNotification');
   let subject;
   if(type === "REMINDER"){
      SSR.compileTemplate('notifyEmail', Assets.getText('email-hangout-reminder.html'));
-     subject = 'CodeBuddies Alert: Hangout Reminder - ' + hangout.topic + ' is scheduled to start within 24 hours!';
+     subject = 'Hangout Reminder - ' + hangout.topic + ' is scheduled to start within 24 hours!';
+  }
+  if(type === "REMINDER_TWO"){
+     SSR.compileTemplate('notifyEmail', Assets.getText('email-hangout-reminder-two.html'));
+     subject = 'Hangout Reminder - ' + hangout.topic + ' is scheduled to start within 2 hours!';
   }
   if(type === "DELETED"){
     SSR.compileTemplate('notifyEmail', Assets.getText('email-hangout-alerts.html'));
-    subject = 'CodeBuddies Alert: Hangout - ' + hangout.topic + ' has been CANCELLED';
+    subject = 'Hangout "' + hangout.topic + '" has been CANCELLED';
+  }
+  if(type === "FOLLOWUP"){
+    SSR.compileTemplate('notifyEmail', Assets.getText('email-hangout-followup.html'));
+    subject = 'What did you learn during "' + hangout.topic + '"?';
   }
 
   const template_data = {
