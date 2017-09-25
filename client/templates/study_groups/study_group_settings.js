021cc07f477c5607416b7f7b6825f926695ad890
lpatmo <linda.peng@alumni.duke.edu>
2017-09-25 00:28:08 -0700

    standardize 'schedule hangout' buttons

--- client/templates/study_groups/study_group_settings.js
+++ client/templates/study_groups/study_group_settings.js
@@ -3,14 +3,31 @@ Template.studyGroupSettings.events({
 
     const studyGroupId = this._id;
 
-    Meteor.call("archiveStudyGroup", studyGroupId ,function (error, result) {
-      if(error){
-        return Bert.alert( error.reason, 'danger', 'growl-top-right' );
-      }
-      if(result){
-        FlowRouter.go("all study groups");
-        return Bert.alert( 'Study Group Archived', 'success', 'growl-top-right' );
-      }
-    })
+    sweetAlert({
+        type: 'warning',
+        title: TAPi18n.__("delete_hangout_confirm"),
+        text: TAPi18n.__("archive_final_warning"),
+        cancelButtonText: TAPi18n.__("no_delete_group"),
+        confirmButtonText: TAPi18n.__("yes_delete_group"),
+        confirmButtonColor: "#d9534f",
+        showCancelButton: true,
+        closeOnConfirm: true,
+      },
+      function() {
+        // disable confirm button to avoid double (or quick) clicking on confirm event
+        swal.disableButtons();
+        // if user confirmed/selected yes, let's call the delete hangout method on the server
+
+
+        Meteor.call("archiveStudyGroup", studyGroupId ,function (error, result) {
+          if(error){
+            return Bert.alert( error.reason, 'danger', 'growl-top-right' );
+          }
+          if(result){
+            FlowRouter.go("all study groups");
+            return Bert.alert( 'Study Group Archived', 'success', 'growl-top-right' );
+          }
+        })
+      });
   }
 });
