00e35ffcbbafe9ae72839ff456b59991f0ba8097
Connie Leung <cleung334@gmail.com>
2017-10-13 07:20:35 +0800

    Use bootstrap to style members table

--- client/templates/study_groups/study_group_online_members.js
+++ client/templates/study_groups/study_group_online_members.js
@@ -1,9 +1,9 @@
 Template.studyGroupOnlineMembers.helpers({
   onlineUsers: function(){
-    let onlineUsernames =  Meteor.users.find({ "status.online": true }, { fields: { username: 1 }})
+    const onlineUsernames =  Meteor.users.find({ "status.online": true }, { fields: { username: 1 }})
                         .fetch()
                         .map(m => m.username);
-    let onlineMembers =  StudyGroups.findOne({_id:FlowRouter.getParam('studyGroupId')})
+    const onlineMembers =  StudyGroups.findOne({_id:FlowRouter.getParam('studyGroupId')})
                     .members
                     .filter(m => onlineUsernames.indexOf(m.name) >= 0);
     return onlineMembers;
