364afb23b2a4ef48b5ce09269026388993b954d0
Connie Leung <cleung334@gmail.com>
2017-10-14 14:05:51 +0800

    Incorporate feedback

--- client/templates/study_groups/study_group_online_members.js
+++ client/templates/study_groups/study_group_online_members.js
@@ -1,12 +1,12 @@
 Template.studyGroupOnlineMembers.helpers({
   onlineUsers: function(){
-    const onlineUsernames =  Meteor.users.find({ "status.online": true }, { fields: { username: 1 }})
+    const onlineUsernames =  Meteor.users.find({ "status.online": true }, { fields: { id: 1 }})
                         .fetch()
-                        .map(m => m.username);
+                        .map(m => m._id);
     const studyGroup = StudyGroups.findOne({_id:FlowRouter.getParam('studyGroupId')});
     if (studyGroup) {
       const onlineMembers =  studyGroup.members
-                    .filter(m => onlineUsernames.indexOf(m.name) >= 0);
+                    .filter(m => onlineUsernames.indexOf(m.id) >= 0);
       return onlineMembers;
     }
     return [];
