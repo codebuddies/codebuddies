43ba619f317e5a8da26d2bc79f0855a1f8efa645
ajparise <ajparise@gmail.com>
2017-11-10 08:44:34 -0500

    Adds database seeder for study groups
    Supplements #267

--- server/seeder/database_seed_remover.js
+++ server/seeder/database_seed_remover.js
@@ -5,6 +5,7 @@ export const databaseSeedRemover = () => {
   Meteor.users.remove({'profile.document_type':'SEED'});
   Hangouts.remove({'document_type':"SEED"});
   Learnings.remove({'document_type':"SEED"});
+  StudyGroups.remove({'document_type':"SEED"});
 
   console.log("seed data removed");
 }
