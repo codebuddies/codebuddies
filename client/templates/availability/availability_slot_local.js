5e6305d726b3d33dcb48df0972ba1d4d06b88dde
distalx <distalx@gmail.com>
2017-11-17 18:10:26 +0530

    pref (availability) readability

--- client/templates/availability/availability_slot_local.js
+++ client/templates/availability/availability_slot_local.js
@@ -3,6 +3,9 @@ import UHR from 'uhr';
 Template.availabilitySlotLocal.helpers({
   localSlotTime: function(){
     const slot = Template.instance().data;
-    return UHR(slot.d, slot.h, slot.m, -1)
+    let result = UHR(slot.d, slot.h, slot.m, -1)
+    result.hour = result.hour < 10  ? `0${result.hour}` : result.hour;
+    result.minute = result.minute < 10  ? `0${result.minute}` : result.minute;
+    return result;
   }
 });
