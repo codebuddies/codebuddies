f7d00507947cec81891ef5ad69bafa414da0050d
distalx <distalx@gmail.com>
2018-01-07 01:15:18 +0530

    inbetweener

---
+++ client/templates/discussion/all_discussions.js
@@ -0,0 +1,11 @@
+Template.allDiscussions.onCreated(function() {
+  const title = "CodeBuddies | Discussions";
+  const metaInfo = {
+    name: "description",
+    content: "CodeBuddies is a community of independent code learners who enjoy sharing knowledge and helping each other learn faster. We come from all over the world; there are members living in the United States, Japan, Sweden, the United Kingdom, Russia, Australia, Canada, India, and more. Everyone is welcome, independent of previous knowledge."
+  };
+
+  DocHead.setTitle(title);
+  DocHead.addMeta(metaInfo);
+
+});
