267bc94d70b4ced840c0f70ed6773e0edcefdb9e
Connie Leung <cleung334@gmail.com>
2018-04-17 22:11:17 +0800

    Add Github signin link

--- client/templates/home/home_logged_out.js
+++ client/templates/home/home_logged_out.js
@@ -12,5 +12,11 @@ Template.homeLoggedOut.events({
       requestPermissions: ['identity.basic', 'identity.email']
     };
     Meteor.loginWithSlack(options);
+  },
+  'click .signInGithub': function(event) {
+    var options = {
+      requestPermissions: ['read:user', 'user:email']
+    };
+    Meteor.loginWithGithub(options);
   }
 });
