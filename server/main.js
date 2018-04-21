767834e393c334bd3cf815387e2395521e28ea50
distalx <distalx@gmail.com>
2018-04-22 04:07:03 +0530

    prettier test commit

--- server/main.js
+++ server/main.js
@@ -1,30 +1,29 @@
-import md5 from 'md5';
-
+import md5 from "md5";
 
 Meteor.startup(function() {
- // fire off cron jobs
+  // fire off cron jobs
   SyncedCron.start();
 
   Accounts.loginServiceConfiguration.remove({
-    service : 'slack'
+    service: "slack"
   });
 
   Accounts.loginServiceConfiguration.remove({
-    service : 'github'
+    service: "github"
   });
 
   Accounts.loginServiceConfiguration.insert({
-    service     : 'slack',
-    "clientId" : Meteor.settings.slack_clientid,
-    "secret" : Meteor.settings.slack_clientsecret,
-    "loginStyle" : "popup"
+    service: "slack",
+    clientId: Meteor.settings.slack_clientid,
+    secret: Meteor.settings.slack_clientsecret,
+    loginStyle: "popup"
   });
 
   Accounts.loginServiceConfiguration.insert({
-    service     : 'github',
-    "clientId" : Meteor.settings.github_clientid,
-    "secret" : Meteor.settings.github_clientsecret,
-    "loginStyle" : "popup"
+    service: "github",
+    clientId: Meteor.settings.github_clientid,
+    secret: Meteor.settings.github_clientsecret,
+    loginStyle: "popup"
   });
 
   smtp = {
@@ -32,80 +31,100 @@ Meteor.startup(function() {
     password: Meteor.settings.private.smtp.password,
     server: Meteor.settings.private.smtp.server,
     port: Meteor.settings.private.smtp.port
+  };
+
+  process.env.MAIL_URL =
+    "smtp://" +
+    encodeURIComponent(smtp.username) +
+    ":" +
+    encodeURIComponent(smtp.password) +
+    "@" +
+    encodeURIComponent(smtp.server) +
+    ":" +
+    smtp.port;
+
+  if (Meteor.users.find().count() === 0) {
+    var password = Random.secret([9]);
+    var id = Accounts.createUser({
+      username: Meteor.settings.root_username,
+      email: Meteor.settings.root_email,
+      password: password
+    });
+    if (id) {
+      Roles.addUsersToRoles(id, "admin", "CB");
+      Email.send({
+        to: Meteor.settings.root_email,
+        from: Meteor.settings.email_from,
+        subject: "With great power comes great responsibility",
+        text: password
+      });
+    }
   }
-
-  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
-
-  if(Meteor.users.find().count()===0){
-
-      var password = Random.secret([9]);
-      var id = Accounts.createUser({
-        username : Meteor.settings.root_username,
-        email : Meteor.settings.root_email,
-        password : password
-      })
-      if(id){
-        Roles.addUsersToRoles(id, 'admin', 'CB');
-        Email.send({
-          to: Meteor.settings.root_email,
-          from: Meteor.settings.email_from,
-          subject: "With great power comes great responsibility",
-          text: password
-        });
-      }
-  }
-
 });
 
-let generateGravatarURL = (email) => {
+let generateGravatarURL = email => {
   const gravatarHash = md5(email.toLowerCase());
-  return{
-    default : Meteor.settings.root_gravatar + gravatarHash + '?size=72',
-    image_192 : Meteor.settings.root_gravatar + gravatarHash + '?size=192',
-    image_512 : Meteor.settings.root_gravatar + gravatarHash + '?size=512'
-  }
-}
+  return {
+    default: Meteor.settings.root_gravatar + gravatarHash + "?size=72",
+    image_192: Meteor.settings.root_gravatar + gravatarHash + "?size=192",
+    image_512: Meteor.settings.root_gravatar + gravatarHash + "?size=512"
+  };
+};
 
-let getRandomUsername = function (username) {
-  const adjectiveList = ['adorable', 'elegant', 'mighty', 'brave', 'fancy', 'fearless', 'magnificent', 'bewildered', 'fierce', 'lazy', 'mysterious', 'worried', 'curious', 'weird', 'cryptic' ];
-  return `${adjectiveList[Math.floor(Math.random() * adjectiveList.length)]}${username}`;
-}
+let getRandomUsername = function(username) {
+  const adjectiveList = [
+    "adorable",
+    "elegant",
+    "mighty",
+    "brave",
+    "fancy",
+    "fearless",
+    "magnificent",
+    "bewildered",
+    "fierce",
+    "lazy",
+    "mysterious",
+    "worried",
+    "curious",
+    "weird",
+    "cryptic"
+  ];
+  return `${
+    adjectiveList[Math.floor(Math.random() * adjectiveList.length)]
+  }${username}`;
+};
 
-let swapUsernameIfExists = function (username) {
-  const usernameExists = Meteor.users.findOne({'username': username});
+let swapUsernameIfExists = function(username) {
+  const usernameExists = Meteor.users.findOne({ username: username });
   if (usernameExists) {
     return swapUsernameIfExists(getRandomUsername(username));
-  }else {
+  } else {
     return username;
   }
-}
+};
 
+let swapUserIfExists = function(email, service, user) {
+  const existingUser = Meteor.users.findOne({ email: email });
 
-let swapUserIfExists = function (email, service, user) {
-
-    const existingUser = Meteor.users.findOne({'email': email});
-
-    if (existingUser) {
-      if (!existingUser.services) {
-        existingUser.services = { resume: { loginTokens: [] }};
-      }
-
-      existingUser.services[service] = user.services[service];
-      user = existingUser;
-      Meteor.users.remove({_id: existingUser._id});
-
-    } else {
-      user.username = swapUsernameIfExists(user.username);
+  if (existingUser) {
+    if (!existingUser.services) {
+      existingUser.services = { resume: { loginTokens: [] } };
     }
 
-    return user;
-}
+    existingUser.services[service] = user.services[service];
+    user = existingUser;
+    Meteor.users.remove({ _id: existingUser._id });
+  } else {
+    user.username = swapUsernameIfExists(user.username);
+  }
+
+  return user;
+};
 
 Accounts.onCreateUser(function(options, user) {
   const service = _.keys(user.services)[0];
 
-  if (service === 'slack') {
-
+  if (service === "slack") {
     const username = options.slack.tokens.user.name;
     const email = options.slack.tokens.user.email;
     const avatar = generateGravatarURL(email);
@@ -114,15 +133,15 @@ Accounts.onCreateUser(function(options, user) {
       avatar: avatar,
       complete: false
     };
-    Roles.setRolesOnUserObj(user, ['user'], 'CB');
+    Roles.setRolesOnUserObj(user, ["user"], "CB");
     user.username = username;
     user.email = email;
     user.profile = profile;
 
-    return  swapUserIfExists(email, service, user)
+    return swapUserIfExists(email, service, user);
   }
 
-  if (service === 'github') {
+  if (service === "github") {
     const email = user.services.github.email;
 
     const avatar = generateGravatarURL(user.services.github.email);
@@ -131,21 +150,20 @@ Accounts.onCreateUser(function(options, user) {
       complete: false
     };
 
-    Roles.setRolesOnUserObj(user, ['user'], 'CB');
+    Roles.setRolesOnUserObj(user, ["user"], "CB");
     user.username = user.services.github.username;
     user.email = user.services.github.email;
     user.profile = profile;
 
-    return swapUserIfExists(email, service, user)
+    return swapUserIfExists(email, service, user);
   }
 
-
-  if (service === 'password') {
+  if (service === "password") {
     const avatar = generateGravatarURL(options.email);
     const profile = {
-      avatar:avatar,
+      avatar: avatar,
       complete: true
-    }
+    };
 
     user.username = user.username;
     user.profile = profile;
@@ -153,23 +171,22 @@ Accounts.onCreateUser(function(options, user) {
 
     return user;
   }
-
 });
 
 // global users observer for app_stats
 Meteor.users.find({ "status.online": true }).observe({
   removed: function(user) {
     //remove participants from active list
-    AppStats.update({"participants.id":user._id},
+    AppStats.update(
+      { "participants.id": user._id },
       {
         $pull: {
           participants: {
-            id : user._id
+            id: user._id
           }
-        },
-
+        }
       },
-      {multi: true});
-
+      { multi: true }
+    );
   }
 });
