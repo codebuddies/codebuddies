2f95d265cad25e1401158b1f611967874ade58a9
Connie Leung <cleung334@gmail.com>
2018-04-28 22:27:05 +0800

    Add global activity feed on home logged out page
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- client/templates/home/home_logged_out.js
+++ client/templates/home/home_logged_out.js
@@ -1,22 +1,64 @@
-Template.homeLoggedOut.onCreated(function(){
+Template.homeLoggedOut.onCreated(function() {
   var title = "CodeBuddies | Home";
-  var metaInfo = {name: "description", content: "We're a community learning code via a Slack chatroom, a Facebook Group, and peer-to-peer organized pair-programming hangouts. Learning with others helps us learn faster. The project is free, open-sourced, and 100% community-built."};
+  var metaInfo = {
+    name: "description",
+    content:
+      "We're a community learning code via a Slack chatroom, a Facebook Group, and peer-to-peer organized pair-programming hangouts. Learning with others helps us learn faster. The project is free, open-sourced, and 100% community-built."
+  };
   DocHead.setTitle(title);
   DocHead.addMeta(metaInfo);
+
+  let instance = this;
+  instance.autorun(function() {
+    instance.subscribe("allStudyGroups", 0, "");
+    instance.subscribe("allDiscussions", 0, "");
+    instance.subscribe("learnings", 0);
+    instance.subscribe("visibleHangouts");
+  });
+
+  instance.studyGroupCount = function() {
+    return StudyGroups.find({}).count();
+  };
+
+  instance.discussionCount = function() {
+    return Discussions.find({}).count();
+  };
+
+  instance.learningsCount = function() {
+    return Learnings.find({}).count();
+  };
+
+  instance.hangoutsCount = function() {
+    return Hangouts.find({}).count();
+  };
 });
 
-
 Template.homeLoggedOut.events({
-  'click .signIn': function(event) {
+  "click .signIn": function(event) {
     var options = {
-      requestPermissions: ['identity.basic', 'identity.email']
+      requestPermissions: ["identity.basic", "identity.email"]
     };
     Meteor.loginWithSlack(options);
   },
-  'click .signInGithub': function(event) {
+  "click .signInGithub": function(event) {
     var options = {
-      requestPermissions: ['read:user', 'user:email']
+      requestPermissions: ["read:user", "user:email"]
     };
     Meteor.loginWithGithub(options);
+  }
+});
+
+Template.homeLoggedOut.helpers({
+  studyGroupCount: function() {
+    return Template.instance().studyGroupCount();
+  },
+  discussionCount: function() {
+    return Template.instance().discussionCount();
+  },
+  learningsCount: function() {
+    return Template.instance().learningsCount();
+  },
+  hangoutsCount: function() {
+    return Template.instance().hangoutsCount();
   }
 });
