aef25bd3feb4a5f9b507e6f9cc73ba72b0edd391
Linda <linda.peng@alumni.duke.edu>
2018-05-16 23:03:48 -0700

    Revert "Release 1.11 (#875)"
    This reverts commit a8e4c27b1078834fe9494cdd737f50d84b07f86f.

--- client/templates/home/home_logged_out.js
+++ client/templates/home/home_logged_out.js
@@ -1,49 +1,21 @@
-Template.homeLoggedOut.onCreated(function() {
+Template.homeLoggedOut.onCreated(function(){
   var title = "CodeBuddies | Home";
-  var metaInfo = {
-    name: "description",
-    content:
-      "We're a community learning code via a Slack chatroom, a Facebook Group, and peer-to-peer organized pair-programming hangouts. Learning with others helps us learn faster. The project is free, open-sourced, and 100% community-built."
-  };
+  var metaInfo = {name: "description", content: "We're a community learning code via a Slack chatroom, a Facebook Group, and peer-to-peer organized pair-programming hangouts. Learning with others helps us learn faster. The project is free, open-sourced, and 100% community-built."};
   DocHead.setTitle(title);
   DocHead.addMeta(metaInfo);
-
-  let instance = this;
-  instance.studyGroupCount = new ReactiveVar(0);
-  instance.discussionCount = new ReactiveVar(0);
-  instance.learningsCount = new ReactiveVar(0);
-  instance.hangoutsCount = new ReactiveVar(0);
-  Meteor.call("getOverallStats", null, function(error, result) {
-    if (error) {
-      console.log(error);
-    } else {
-      const {
-        studyGroupCount = 0,
-        discussionCount = 0,
-        learningsCount = 0,
-        hangoutsCount = 0
-      } =
-        result || {};
-      console.log(studyGroupCount);
-
-      instance.studyGroupCount.set(studyGroupCount);
-      instance.discussionCount.set(discussionCount);
-      instance.learningsCount.set(learningsCount);
-      instance.hangoutsCount.set(hangoutsCount);
-    }
-  });
 });
 
+
 Template.homeLoggedOut.events({
-  "click .signIn": function(event) {
+  'click .signIn': function(event) {
     var options = {
-      requestPermissions: ["identity.basic", "identity.email"]
+      requestPermissions: ['identity.basic', 'identity.email']
     };
     Meteor.loginWithSlack(options);
   },
-  "click .signInGithub": function(event) {
+  'click .signInGithub': function(event) {
     var options = {
-      requestPermissions: ["read:user", "user:email"]
+      requestPermissions: ['read:user', 'user:email']
     };
     Meteor.loginWithGithub(options);
   }
--- server/hangouts/publications.js
+++ server/hangouts/publications.js
@@ -1,60 +1,51 @@
 Meteor.publish("hangouts", function(limit) {
   check(limit, Number);
 
-  if (Roles.userIsInRole(this.userId, ["admin", "moderator"], "CB")) {
-    return Hangouts.find(
-      {},
-      { fields: { email_addresses: 0 }, sort: { start: -1 }, limit: limit }
-    );
+  if (Roles.userIsInRole(this.userId, ['admin','moderator'], 'CB')) {
+    return Hangouts.find({}, {fields:{'email_addresses': 0 }, sort: { start: -1 }, 'limit':limit});
   } else {
-    return Hangouts.find(
-      { visibility: { $ne: false } },
-      { fields: { email_addresses: 0 }, sort: { start: -1 }, limit: limit }
-    );
+    return Hangouts.find({'visibility':{$ne:false}}, {fields:{'email_addresses': 0 }, sort: { start: -1 }, 'limit':limit});
   }
 
   this.ready();
 });
 
+
+
 Meteor.publish("hangoutById", function(hangoutId) {
-  if (Roles.userIsInRole(this.userId, ["admin", "moderator"], "CB")) {
-    return Hangouts.find(
-      { _id: hangoutId },
-      { fields: { email_addresses: 0 } }
-    );
+  if (Roles.userIsInRole(this.userId, ['admin','moderator'], 'CB')) {
+    return Hangouts.find({_id: hangoutId},
+                         {fields:{'email_addresses': 0 }});
   } else {
-    return Hangouts.find(
-      { _id: hangoutId, visibility: { $ne: false } },
-      { fields: { email_addresses: 0 } }
-    );
+    return Hangouts.find({_id: hangoutId, 'visibility':{ $ne: false} },
+                         {fields: {'email_addresses': 0 }} );
   }
 });
+
 
 Meteor.publish("hangoutsJoined", function(limit, userId) {
   check(limit, Number);
   check(userId, String);
 
-  if (this.userId) {
-    return Hangouts.find(
-      { users: { $elemMatch: { $eq: userId } }, visibility: { $ne: false } },
-      { sort: { timestamp: -1 }, limit: limit }
-    );
+  if(this.userId) {
+    return Hangouts.find({users:{ $elemMatch:{ $eq: userId}},'visibility':{ $ne: false}},
+                         {sort: {timestamp: -1}, limit: limit});
   } else {
-    this.ready();
+   this.ready();
   }
+
 });
+
 
 Meteor.publish("studyGroupHangouts", function(groupId, limit) {
   check(groupId, String);
-  return Hangouts.find(
-    { visibility: { $ne: false }, "group.id": groupId },
-    { sort: { start: -1 }, limit: limit }
-  );
+  return Hangouts.find({ visibility: {$ne:false}, 'group.id': groupId}, {sort: {start: -1}, limit:limit});
 });
 
-Meteor.publish("hangoutSearch", function(searchTerm) {
+Meteor.publish( 'hangoutSearch', function(searchTerm) {
   check(searchTerm, String);
-  if (_.isEmpty(searchTerm)) return this.ready();
+    if (_.isEmpty(searchTerm))
+    return this.ready();
 
-  return Hangouts.search(searchTerm);
+    return Hangouts.search(searchTerm);
 });
