ba21c9bc577c22ec3c0c06fd0681aa10b3036d15
Linda <linda.peng@alumni.duke.edu>
2018-05-16 23:05:31 -0700

    Revert "Release 1.11 (#875)" (#878)
    This reverts commit a8e4c27b1078834fe9494cdd737f50d84b07f86f.

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,208 +1,173 @@
 if (Meteor.isClient) {
-  Meteor.startup(function() {
-    $("head").append(
-      '<script src="https://meet.jit.si/external_api.js"></script>'
-    );
+  Meteor.startup(function () {
 
-    if (!Meteor.settings.public.isModeDebug) {
+    $('head').append('<script src="https://meet.jit.si/external_api.js"></script>');
+
+    if(!Meteor.settings.public.isModeDebug){
       console = console || {};
-      console.log = function() {};
+      console.log = function(){};
     }
 
-    const defaultLang = "en";
-    const localStorageLang = localStorage.getItem("languageCode");
-    const browserLang = (
-      window.navigator.userLanguage ||
-      window.navigator.language ||
-      ""
-    ).slice(0, 2);
+    const defaultLang = 'en'
+    const localStorageLang = localStorage.getItem('languageCode');
+    const browserLang = (window.navigator.userLanguage || window.navigator.language || '').slice(0,2)
     TAPi18n.setLanguage(localStorageLang || browserLang || defaultLang)
-      .fail(console.log)
-      .always(() =>
-        localStorage.setItem("languageCode", TAPi18n.getLanguage())
-      );
+    .fail(console.log)
+    .always(() => localStorage.setItem('languageCode', TAPi18n.getLanguage()))
   });
 }
 
-Template.registerHelper("equals", function(a, b) {
-  return a === b;
+Template.registerHelper('equals', function (a, b) {
+      return a === b;
 });
 
-Template.registerHelper("cleanDateFormatCalendar", function(date) {
+Template.registerHelper('cleanDateFormatCalendar', function(date) {
   return moment(date).calendar();
 });
 
-Template.registerHelper("isUserCommentAuthor", function(authorId) {
-  return Meteor.userId() === authorId ? true : false;
+Template.registerHelper("isUserCommentAuthor", function(authorId){
+  return  Meteor.userId() === authorId ? true : false ;
 });
 
-Template.registerHelper("totalVotes", function(upvote = 0, downvote = 0) {
-  return upvote - downvote;
+Template.registerHelper("totalVotes", function(upvote = 0, downvote = 0){
+  return  upvote - downvote;
 });
 
-Template.registerHelper("getHangoutTypeSign", function(hangoutType) {
-  if (hangoutType == "silent") {
-    return "fa-microphone-slash text-danger-color";
-  } else if (hangoutType == "teaching") {
-    return "fa-user text-warning-color";
-  } else if (hangoutType == "collaboration") {
-    return "fa-users text-success-color";
+Template.registerHelper("getHangoutTypeSign", function(hangoutType){
+  if (hangoutType == 'silent') {
+    return 'fa-microphone-slash text-danger-color';
+  } else if (hangoutType == 'teaching') {
+    return 'fa-user text-warning-color';
+  } else if (hangoutType == 'collaboration') {
+    return 'fa-users text-success-color';
   }
 });
 
-Template.registerHelper("getHangoutGoogleCalendarDate", function(date) {
+Template.registerHelper("getHangoutGoogleCalendarDate", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("YYYYMMDD");
+  return moment(date).tz(tz).format('YYYYMMDD');
 });
 
-Template.registerHelper("getHangoutGoogleCalendarTime", function(date) {
+Template.registerHelper("getHangoutGoogleCalendarTime", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("HHmmss");
+  return moment(date).tz(tz).format('HHmmss');
 });
 
-Template.registerHelper("getHangoutStartDateTime", function(date) {
+Template.registerHelper("getHangoutStartDateTime", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("dddd MMMM Do YYYY, h:mm a z");
+  return moment(date).tz(tz).format('dddd MMMM Do YYYY, h:mm a z');
 });
 
-Template.registerHelper("getHangoutStartDateDay", function(date) {
+Template.registerHelper("getHangoutStartDateDay", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("dddd MMMM Do YYYY");
+  return moment(date).tz(tz).format('dddd MMMM Do YYYY');
 });
 
-Template.registerHelper("getHangoutStartTime", function(date) {
+Template.registerHelper("getHangoutStartTime", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("h:mm a z");
+  return moment(date).tz(tz).format('h:mm a z');
 });
 
-Template.registerHelper("getHangoutEndDateTime", function(date) {
+Template.registerHelper("getHangoutEndDateTime", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("MMMM Do h:mm a z");
+  return moment(date).tz(tz).format('MMMM Do h:mm a z')
 });
 
-Template.registerHelper("getHangoutEndTime", function(date) {
+Template.registerHelper("getHangoutEndTime", function(date){
   const tz = TimezonePicker.detectedZone();
-  return moment(date)
-    .tz(tz)
-    .format("h:mm a z");
+  return moment(date).tz(tz).format('h:mm a z')
 });
 
-Template.registerHelper("isHangoutUpcoming", function(startDate) {
+Template.registerHelper('isHangoutUpcoming', function(startDate) {
   return startDate > new Date() ? true : false;
 });
 
-Template.registerHelper("isHangoutInProgress", function(startDate, endDate) {
-  return startDate <= new Date() && endDate >= new Date() ? true : false;
+Template.registerHelper("isHangoutInProgress", function(startDate, endDate){
+  return (startDate <= new Date() && endDate >= new Date()) ? true : false;
 });
 
-Template.registerHelper("isHangoutCompleted", function(endDate) {
+Template.registerHelper('isHangoutCompleted', function(endDate) {
   return endDate < new Date() ? true : false;
 });
 
-Template.registerHelper("isAttending", function(users) {
+Template.registerHelper("isAttending", function(users){
   return users.indexOf(Meteor.userId()) != -1;
 });
 
-Template.registerHelper("upcomingTime", function(start) {
-  return start > new Date()
-    ? TAPi18n.__("upcoming_time", { time: moment(start).fromNow() })
-    : "The hangout has started!";
+Template.registerHelper("upcomingTime", function(start){
+  return start > new Date() ? TAPi18n.__("upcoming_time", { time: moment(start).fromNow() }) : "The hangout has started!" ;
 });
 
-Template.registerHelper("isHangoutEndTimeTBA", function(start, end) {
-  const duration = (end - start) / (1000 * 60 * 60 * 24);
-  return duration === 1 ? true : false;
+Template.registerHelper("isHangoutEndTimeTBA", function(start, end){
+  const duration = (end - start) / (1000 * 60 * 60 * 24)
+  return duration === 1 ?  true : false;
+
 });
 
-Template.registerHelper("isOwnerOfTheGroup", function(userId, groupId) {
+Template.registerHelper("isOwnerOfTheGroup", function(userId, groupId){
   const loggedInUserId = Meteor.userId();
 
-  return loggedInUserId !== userId &&
-    Roles.userIsInRole(loggedInUserId, ["owner"], groupId)
-    ? true
-    : false;
+  return ((loggedInUserId !== userId) &&  (Roles.userIsInRole( loggedInUserId, ['owner' ], groupId)) ? true : false  ) ;
+
 });
 
-Template.registerHelper("canUpdateUserRoleForGroup", function(
-  subjectId,
-  groupId,
-  subjectRole
-) {
+Template.registerHelper("canUpdateUserRoleForGroup", function(subjectId, groupId, subjectRole){
   const loggedInUserId = Meteor.userId();
 
-  return loggedInUserId !== subjectId &&
-    (subjectRole !== "owner" && subjectRole !== "admin") &&
-    Roles.userIsInRole(loggedInUserId, ["owner", "admin"], groupId)
-    ? true
-    : false;
+  return ((loggedInUserId !== subjectId) && (subjectRole !== 'owner' && subjectRole !=='admin') &&  (Roles.userIsInRole( loggedInUserId, ['owner', 'admin' ], groupId)) ? true : false  ) ;
+
 });
 
-Template.registerHelper("isOrganizers", function(role) {
-  return ["owner", "admin", "moderator"].indexOf(role) < 0 ? false : true;
+Template.registerHelper("isOrganizers", function(role){
+
+  return ["owner", "admin", "moderator"].indexOf(role) < 0 ? false : true ;
 });
 
-Template.registerHelper("slotDayString", function(day) {
+Template.registerHelper("slotDayString", function(day){
   switch (day) {
     case 00:
-      return "MON";
+      return "MON"
       break;
     case 01:
-      return "TUE";
+      return "TUE"
       break;
     case 02:
-      return "WED";
+      return "WED"
       break;
     case 03:
-      return "THU";
+      return "THU"
       break;
     case 04:
-      return "FRI";
+      return "FRI"
       break;
     case 05:
-      return "SAT";
+      return "SAT"
       break;
     case 06:
-      return "SUN";
+      return "SUN"
       break;
     default:
-      return "NaN";
+      return "NaN"
   }
 });
 
-Template.registerHelper("isAuthor", function(userId) {
+Template.registerHelper("isAuthor", function(userId){
   return Meteor.userId() === userId;
 });
 
-Template.registerHelper("inList", function(list, item) {
+Template.registerHelper('inList', function(list, item) {
   if (list) {
     return list.indexOf(item) != -1;
   }
   return false;
 });
 
-Template.registerHelper("isInCollection", function(collection) {
-  const actor = _.find(collection, function(item) {
-    return item.id === Meteor.userId();
-  });
+Template.registerHelper('isInCollection', function(collection) {
+  const actor = _.find(collection, function(item) { return item.id === Meteor.userId() ; });
   return actor ? true : false;
 });
 
-Template.registerHelper("truncateIt", function(text, length) {
-  return text.truncate(length);
-});
 
-Template.registerHelper("instance", function() {
-  return Template.instance();
+Template.registerHelper("truncateIt", function(text, length){
+  return text.truncate(length)
 });
--- server/monti.js
+++
@@ -1,7 +0,0 @@
-// only running in production.
-if (Meteor.settings.isModeProduction) {
-  Kadira.connect(
-    Meteor.settings.private.kadira.appId,
-    Meteor.settings.private.kadira.appSecret
-  );
-}
