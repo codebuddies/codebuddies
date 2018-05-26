c4883bf7f03302b557348db05e5e13ecc1b2a998
distalx <distalx@gmail.com>
2018-05-26 15:17:36 +0530

    feat(unsub-link): add unsubscribe link to email templates
    unsubscribe link for new_discussion, discussion_thread, new_member & new_hangout email notification.

---
+++ client/templates/settings/unsubscribe_from_mailing_list.js
@@ -0,0 +1,39 @@
+// import { ReactiveVar } from 'meteor/reactive-var';
+import { UnsubscribeLinks } from "../../../imports/api/unsubscribe_links/unsubscribe_links";
+Template.unsubscribeFromMailingList.onCreated(function() {
+  const instance = this;
+  instance.processing = new ReactiveVar(true);
+  instance.subscribe(
+    "unsubscribe.link",
+    FlowRouter.getParam("unsubscribeLinkId")
+  );
+});
+
+Template.unsubscribeFromMailingList.helpers({
+  unsubscribeLink() {
+    return UnsubscribeLinks.findOne({
+      _id: FlowRouter.getParam("unsubscribeLinkId")
+    });
+  }
+});
+
+Template.unsubscribeFromMailingList.events({
+  "click #updateEmailPreferences"(event, template) {
+    const unsubscribeLinkId = FlowRouter.getParam("unsubscribeLinkId");
+    template.processing.set(true);
+    Meteor.call("unsubscribe.me", unsubscribeLinkId, function(error, result) {
+      if (error) {
+        template.processing.set(false);
+        Bert.alert(error.reason, "danger", "growl-top-right");
+      }
+      if (result) {
+        FlowRouter.go("/");
+        template.processing.set(false);
+        Bert.alert("Successfully Unsubscribed", "success", "growl-top-right");
+      }
+    });
+  },
+  "click #cancel"(event, template) {
+    FlowRouter.go("/");
+  }
+});
