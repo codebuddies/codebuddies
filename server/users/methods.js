e8e28ec6136e5a8a366065f72ff3bcba8c0f2acb
Linda <linda.peng@alumni.duke.edu>
2018-05-26 01:54:00 -0700

    Issue 799: Add an opt-in checkbox to the nav for privacy policy (#887)
    * update terms of service and privacy policy
    
    * add export link that downloads user profile data into a CSV
    
    * add privacy agreement checkbox that shows for all users until they check to agree
    
    * add privacy agremeent templateset
    
    * proofreading

--- server/users/methods.js
+++ server/users/methods.js
@@ -1,10 +1,12 @@
-import { check, Match } from 'meteor/check'
+import { check, Match } from "meteor/check";
 
 Meteor.methods({
-  getUserDetails : function(userId){
+  getUserDetails: function(userId) {
     check(userId, String);
-    return Meteor.users.findOne({_id:userId},
-                                {fields: { emails: 0, services: 0, roles: 0, email: 0}});
+    return Meteor.users.findOne(
+      { _id: userId },
+      { fields: { emails: 0, services: 0, roles: 0, email: 0 } }
+    );
   },
 
   setUserProfile: function(profileInfo) {
@@ -18,78 +20,114 @@ Meteor.methods({
     };
     check(profileInfo, pattern);
     if (!this.userId) {
-      throw new Meteor.Error('users.methods.setUserProfile.not-logged-in', 'Must be logged in.');
+      throw new Meteor.Error(
+        "users.methods.setUserProfile.not-logged-in",
+        "Must be logged in."
+      );
     }
-    Meteor.users.update({_id: Meteor.userId()},
-                        {$set: {
-                          'profile.bio': profileInfo.bio,
-                          'profile.website': profileInfo.website,
-                          'profile.social.twitter': profileInfo.twitter,
-                          'profile.social.github': profileInfo.github,
-                          'profile.social.facebook': profileInfo.facebook,
-                          'profile.social.linkedin': profileInfo.linkedin
-                        }});
+    Meteor.users.update(
+      { _id: Meteor.userId() },
+      {
+        $set: {
+          "profile.bio": profileInfo.bio,
+          "profile.website": profileInfo.website,
+          "profile.social.twitter": profileInfo.twitter,
+          "profile.social.github": profileInfo.github,
+          "profile.social.facebook": profileInfo.facebook,
+          "profile.social.linkedin": profileInfo.linkedin
+        }
+      }
+    );
   },
 
   setUserStatus: function(currentStatus) {
     check(currentStatus, String);
     if (!this.userId) {
-      throw new Meteor.Error('users.methods.setUserStatus.not-logged-in', 'Must be logged in.');
+      throw new Meteor.Error(
+        "users.methods.setUserStatus.not-logged-in",
+        "Must be logged in."
+      );
     }
-    Meteor.users.update({_id: Meteor.userId()},
-                        {$set: {statusMessage: currentStatus, statusDate: new Date()}});
+    Meteor.users.update(
+      { _id: Meteor.userId() },
+      { $set: { statusMessage: currentStatus, statusDate: new Date() } }
+    );
   },
-
+  setPrivacyResponse: function(isChecked) {
+    check(isChecked, Boolean);
+    if (!this.userId) {
+      throw new Meteor.Error(
+        "users.methods.setUserStatus.not-logged-in",
+        "Must be logged in."
+      );
+    }
+    Meteor.users.update(
+      { _id: Meteor.userId() },
+      { $set: { "profile.privacyResponse": isChecked } }
+    );
+    return true;
+  },
   setHangoutStatus: function(hangoutStatus) {
     check(hangoutStatus, String);
     if (!this.userId) {
-      throw new Meteor.Error('users.methods.setHangoutStatus.not-logged-in', 'Must be logged in.');
+      throw new Meteor.Error(
+        "users.methods.setHangoutStatus.not-logged-in",
+        "Must be logged in."
+      );
     }
-    Meteor.users.update({_id: Meteor.userId()},
-                        {$set: {statusHangout: hangoutStatus}});
+    Meteor.users.update(
+      { _id: Meteor.userId() },
+      { $set: { statusHangout: hangoutStatus } }
+    );
   },
 
   getHangoutsJoinedCount: function(userId) {
     check(userId, String);
-    return Hangouts.find({users:{$elemMatch:{$eq:userId}},'visibility':{$ne:false}}).count();
+    return Hangouts.find({
+      users: { $elemMatch: { $eq: userId } },
+      visibility: { $ne: false }
+    }).count();
   }
 });
 
 /**
-* Update Emails Preferences
-* @function
-* @name updateEmailsPreference
-* @param { Object } - data
-* @return {Boolean} true on success
-*/
+ * Update Emails Preferences
+ * @function
+ * @name updateEmailsPreference
+ * @param { Object } - data
+ * @return {Boolean} true on success
+ */
 Meteor.methods({
-  updateEmailsPreference:function(data){
-    check(data,{
+  updateEmailsPreference: function(data) {
+    check(data, {
       emails_preference: Match.Maybe([String])
     });
     if (!this.userId) {
-      throw new Meteor.Error('users.methods.updateEmailsPreference.not-logged-in', 'Must be logged in.');
+      throw new Meteor.Error(
+        "users.methods.updateEmailsPreference.not-logged-in",
+        "Must be logged in."
+      );
     }
 
-    Meteor.users.update({_id: Meteor.userId()},
-                        {$set: { emails_preference: data.emails_preference } } );
+    Meteor.users.update(
+      { _id: Meteor.userId() },
+      { $set: { emails_preference: data.emails_preference } }
+    );
 
     return true;
-
   }
 });
 
-
 /**
-* Update Basic Information
-* @function
-* @name updateEmailsPreference
-* @param { Object } - data
-* @return {Boolean} true on success
-*/
+ * Update Basic Information
+ * @function
+ * @name updateEmailsPreference
+ * @param { Object } - data
+ * @return {Boolean} true on success
+ */
 Meteor.methods({
-  updateBasicInformation:function(data){
-    check(data,{
+  updateBasicInformation: function(data) {
+    check(data, {
       firstname: String,
       lastname: String,
       username: String
@@ -97,28 +135,37 @@ Meteor.methods({
 
     const actorId = Meteor.userId();
     if (!actorId) {
-      throw new Meteor.Error('users.methods.updateBasicInformation.not-logged-in', 'Must be logged in.');
+      throw new Meteor.Error(
+        "users.methods.updateBasicInformation.not-logged-in",
+        "Must be logged in."
+      );
     }
 
     // Check username is unique
-    const regex = new RegExp(`^${data.username}$`, 'i');
-    const isUsernameInUse = Meteor.users.findOne({ '_id': { $ne: actorId }, 'username': regex });
+    const regex = new RegExp(`^${data.username}$`, "i");
+    const isUsernameInUse = Meteor.users.findOne({
+      _id: { $ne: actorId },
+      username: regex
+    });
     if (isUsernameInUse) {
-      throw new Meteor.Error('Users.methods.updateBasicInformation.username-is-already-in-use', 'Username is already in user.');
+      throw new Meteor.Error(
+        "Users.methods.updateBasicInformation.username-is-already-in-use",
+        "Username is already in user."
+      );
     }
 
-    Meteor.users.update({_id: actorId},
+    Meteor.users.update(
+      { _id: actorId },
       {
         $set: {
-          'username': data.username,
-          'profile.firstname': data.firstname,
-          'profile.lastname': data.lastname,
-          'profile.complete': true
+          username: data.username,
+          "profile.firstname": data.firstname,
+          "profile.lastname": data.lastname,
+          "profile.complete": true
         }
       }
     );
 
     return true;
-
   }
 });
