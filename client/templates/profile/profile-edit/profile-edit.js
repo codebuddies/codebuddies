38543a844f06d50466ee0eec7596e81c18fb13ab
distalx <distalx@users.noreply.github.com>
2018-06-17 13:12:46 +0530

    add support for `buy me a coffee` and other links. (#908)
    * Release 1.12 Hotfixes (#901)
    
    * Hotfix: `startWithVideoMuted` (#905)
    
    * feat(user profile): allow user to add following links to their profile  `buymeacoffee`, `patreon` or
    
    * feat(hangout page): display support links on hangout page, if organizer has any.
    
    * small wording tweaks
    
    * style(users): remove console.log

--- client/templates/profile/profile-edit/profile-edit.js
+++ client/templates/profile/profile-edit/profile-edit.js
@@ -7,40 +7,46 @@ Template.profileEdit.helpers({
 
 Template.profileEdit.events({
   // Track text for character counting
-  'keyup #bio-text': function(event) {
+  "keyup #bio-text": function(event) {
     var currentLength = $("#bio-text").val().length;
     bioCharCount = 140 - currentLength;
-    $('.bioCharLeft').text(bioCharCount + ' characters left');
+    $(".bioCharLeft").text(bioCharCount + " characters left");
 
     if (currentLength >= 130) {
-      $('.bioCharLeft').css("color", "#ff9980");
+      $(".bioCharLeft").css("color", "#ff9980");
     } else {
-      $('.bioCharLeft').css("color", "white");
+      $(".bioCharLeft").css("color", "white");
     }
   },
 
-  'click #updateProfile': function() {
+  "click #updateProfile": function() {
     var profileInfo = {
       location: $('input[name="location"]').val(),
-      bio: $('#bio-text').val(),
+      bio: $("#bio-text").val(),
       website: $('input[name="website"]').val(),
       twitter: $('input[name="twitter"]').val(),
       github: $('input[name="github"]').val(),
       facebook: $('input[name="facebook"]').val(),
-      linkedin: $('input[name="linkedin"]').val()
+      linkedin: $('input[name="linkedin"]').val(),
+      buymeacoffee: $('input[name="buymeacoffee"]').val(),
+      patreon: $('input[name="patreon"]').val(),
+      nonprofit: $('input[name="nonprofit"]').val()
     };
+    console.log("profileInfo ", profileInfo);
 
-    Meteor.call('setUserProfile', profileInfo, function(error, result) {});
+    Meteor.call("setUserProfile", profileInfo, function(error, result) {});
 
     // reset form fields
-    $('input[name="location"]').val('');
-    $('#bio-text').val('');
+    $('input[name="location"]').val("");
+    $("#bio-text").val("");
     $('input[name="website"]').val();
-    $('input[name="twitter"]').val('');
-    $('input[name="github"]').val('');
-    $('input[name="facebook"]').val('');
-    $('input[name="linkedin"]').val('');
+    $('input[name="twitter"]').val("");
+    $('input[name="github"]').val("");
+    $('input[name="facebook"]').val("");
+    $('input[name="patreon"]').val("");
+    $('input[name="buymeacoffee"]').val("");
+    $('input[name="nonprofit"]').val("");
 
-    Session.set('editMode',false);
+    Session.set("editMode", false);
   }
 });
