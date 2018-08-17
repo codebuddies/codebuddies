580b196babc92e4f360697babf97674427fdbdaf
Linda <linda.peng@alumni.duke.edu>
2018-08-17 02:23:34 -0700

    Hotfixes to "external hangout link" issue (#942)
    * added HTML elements
    
    * move external checkbox over to hangout modal instead of displaying on post itself
    
    * added HTML elements
    
    * move external checkbox over to hangout modal instead of displaying on post itself
    
    * support external hangout links on hangout create/edit/clone modals
    
    * Fix bug re: if user edits or duplicates hangout from the hangout action bar (external link gets overridden). Also make sure external link shows without RSVP if one exists.
    
    * Update versions

--- client/templates/hangout/hangout_action_bar.js
+++ client/templates/hangout/hangout_action_bar.js
@@ -1,38 +1,47 @@
-import QuillEditor from '../../libs/QuillEditor';
+import QuillEditor from "../../libs/QuillEditor";
 
 Template.hangoutActionBar.events({
-  'click .report-hangout': function(e, hangout) {
-
+  "click .report-hangout": function(e, hangout) {
     if (Meteor.userId()) {
       const hangoutId = hangout.data._id;
-      Session.set('hangoutId', hangoutId);
+      Session.set("hangoutId", hangoutId);
 
-      Modal.show('reportHangoutModal');
+      Modal.show("reportHangoutModal");
     }
-
   },
-  'click .clone-hangout': function(e, hangout) {
-
+  "click .clone-hangout": function(e, hangout) {
     if (Meteor.userId()) {
       // console.log(hangout.data._id + ' this is a cloned hangout id');
-      Session.set('hangoutId', hangout.data._id);
-      Modal.show('cloneHangoutModal', {hangout});
-      $('#clone-hangout-modal #topic').val(hangout.data.topic);
-      $('#clone-hangout-modal input[value=' + hangout.data.type + ']').prop("checked", true);
+      Session.set("hangoutId", hangout.data._id);
+      Modal.show("cloneHangoutModal", { hangout });
+      $("#clone-hangout-modal input#external-checkbox").prop(
+        "checked",
+        hangout.data.externalCheckbox
+      );
+      $("#clone-hangout-modal #externalButtonText").val(
+        hangout.data.externalButtonText
+      );
+      $("#clone-hangout-modal #externalURL").val(hangout.data.externalURL);
+      $("#clone-hangout-modal #topic").val(hangout.data.topic);
+      $("#clone-hangout-modal input[value=" + hangout.data.type + "]").prop(
+        "checked",
+        true
+      );
     }
-
   },
-  'click .edit-hangout': function(e, hangout) {
+  "click .edit-hangout": function(e, hangout) {
     //console.log(hangout.data.topic);
     //pass in the right times like 03/09/2016 2:03 AM
-    var start_time_reverted = moment(hangout.data.start).format('MM/DD/YYYY h:mm A');
-    var hangoutDuration = hangout.data.duration
+    var start_time_reverted = moment(hangout.data.start).format(
+      "MM/DD/YYYY h:mm A"
+    );
+    var hangoutDuration = hangout.data.duration;
 
     //var end_time_reverted = moment(hangout.data.end).format('MM/DD/YYYY h:mm A');
 
-    console.log(hangout.data._id + ' this is an edited hangout id');
+    console.log(hangout.data._id + " this is an edited hangout id");
 
-    Session.set('hangoutId', hangout.data._id);
+    Session.set("hangoutId", hangout.data._id);
     // Session.set('hostId', hangout.data.user_id);
     // Session.set('hostUsername', hangout.data.creator);
 
@@ -44,51 +53,67 @@ Template.hangoutActionBar.events({
     // console.log(content);
     //console.log(editor_content.html());
 
-    Modal.show('editHangoutModal', {hangout});
-    $('#edit-hangout-modal #topic').val(hangout.data.topic);
+    Modal.show("editHangoutModal", { hangout });
+    $("#edit-hangout-modal input#external-checkbox").prop(
+      "checked",
+      hangout.data.externalCheckbox
+    );
+    $("#edit-hangout-modal #externalButtonText").val(
+      hangout.data.externalButtonText
+    );
+    $("#edit-hangout-modal #externalURL").val(hangout.data.externalURL);
+    $("#edit-hangout-modal #topic").val(hangout.data.topic);
     // $('#edit-hangout-modal #description').val(hangout.data.description);
     // templateInstance.editor.setContents(hangout.data.description);
-    $('#edit-hangout-modal input[value=' + hangout.data.type + ']').prop("checked", true);
-    $('#edit-hangout-modal #start-date-time').val(start_time_reverted);
-    $('#edit-hangout-modal #end-date-time').val(hangoutDuration);
-    $('#edit-hangout-modal #group').val(hangout.data.group.title);
+    $("#edit-hangout-modal input[value=" + hangout.data.type + "]").prop(
+      "checked",
+      true
+    );
+    $("#edit-hangout-modal #start-date-time").val(start_time_reverted);
+    $("#edit-hangout-modal #end-date-time").val(hangoutDuration);
+    $("#edit-hangout-modal #group").val(hangout.data.group.title);
     //console.log(start_time_reverted);
     //console.log(end_time_reverted);
-
   },
-  'click .delete-hangout': function(e, hangout) {
-
+  "click .delete-hangout": function(e, hangout) {
     const data = {
       hangoutId: this._id,
       hostId: this.host.id,
-      hostUsername: this.host.name,
-    }
+      hostUsername: this.host.name
+    };
 
-
-    sweetAlert({
-        type: 'warning',
+    sweetAlert(
+      {
+        type: "warning",
         title: TAPi18n.__("delete_hangout_confirm"),
         text: TAPi18n.__("delete_hangout_text"),
         cancelButtonText: TAPi18n.__("no_delete_hangout"),
         confirmButtonText: TAPi18n.__("yes_delete_hangout"),
         confirmButtonColor: "#d9534f",
         showCancelButton: true,
-        closeOnConfirm: false,
+        closeOnConfirm: false
       },
       function() {
         // disable confirm button to avoid double (or quick) clicking on confirm event
         swal.disableButtons();
         // if user confirmed/selected yes, let's call the delete hangout method on the server
 
-        Meteor.call('deleteHangout', data, function(error, result) {
+        Meteor.call("deleteHangout", data, function(error, result) {
           if (result) {
-            swal("Poof!", "Your hangout has been successfully deleted!", "success");
+            swal(
+              "Poof!",
+              "Your hangout has been successfully deleted!",
+              "success"
+            );
           } else {
-            swal("Oops something went wrong!", error.error + "\n Try again", "error");
+            swal(
+              "Oops something went wrong!",
+              error.error + "\n Try again",
+              "error"
+            );
           }
         });
-      }); //sweetAlert
-  },
-
-
+      }
+    ); //sweetAlert
+  }
 });
