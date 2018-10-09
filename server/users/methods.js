9940cbc019efd309d218b19cfd451327c8a7039a
Connie Leung <cleung334@gmail.com>
2018-10-09 15:50:39 +0800

    Issue 948 default email settings (#995)
    * feat (email notification) add notification flag
    
    * Default email preferences for new user
    
    Signed-off-by: Connie Leung <cleung334@gmail.com>
    
    * Work on unsubscribe from mailing list email template
    
    * Update SparkPost smtp server password
    
    * perf(unsubscribe.me): workflow improvement

--- server/users/methods.js
+++ server/users/methods.js
@@ -1,5 +1,5 @@
 import { check, Match } from "meteor/check";
-
+import { UnsubscribeLinks } from "../../imports/api/unsubscribe_links/unsubscribe_links";
 Meteor.methods({
   getUserDetails: function(userId) {
     check(userId, String);
@@ -194,7 +194,6 @@ Meteor.methods({
  * @param { String } - userId
  * @return {Object}
  */
-
 Meteor.methods({
   "users.getSupportLink"(userId) {
     check(userId, String);
@@ -208,5 +207,24 @@ Meteor.methods({
     } else {
       return null;
     }
+  }
+});
+
+/**
+ * get users email Preferences
+ * @function
+ * @name users.getEmailPreferences
+ * @param { String } - unsubscribeLinkId
+ * @return { Array } - array of current preference.
+ */
+Meteor.methods({
+  async "users.getEmailPreferences"(unsubscribeLinkId) {
+    check(unsubscribeLinkId, String);
+
+    const { recipient_id = null } =
+      (await UnsubscribeLinks.findOne({ _id: unsubscribeLinkId })) || {};
+    const user = await Meteor.users.findOne({ _id: recipient_id });
+
+    return (user && user.emails_preference) || [];
   }
 });
