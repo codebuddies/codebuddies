18bd5e7d4dde9d902cdf8606cee60acfe5717eee
Robert Mcelvenny <thunatos@Roberts-Mac-mini.local>
2018-10-25 18:32:30 -0700

    updated layout.js for slack sweetAlert2 fix

--- client/layouts/layout.js
+++ client/layouts/layout.js
@@ -1,24 +1,37 @@
 Template.layout.events({
-  "click .continue-popup": function(event, template){
+  "click .continue-popup": function(event, template) {
     event.preventDefault();
     if (!Meteor.userId()) {
-      sweetAlert({
-        imageUrl: '/images/slack-signin-example.jpg',
-        imageSize: '140x120',
+      swal({
+        imageUrl: "/images/slack-signin-example.jpg",
+        imageSize: "140x120",
         title: TAPi18n.__("you_are_almost_there"),
         html: TAPi18n.__("continue_popup_text"),
         showCancelButton: true,
         confirmButtonText: TAPi18n.__("continue_with_slack"),
-        cancelButtonText: TAPi18n.__("not_now"),
-
-      },
-      function(){
+        cancelButtonText: TAPi18n.__("not_now")
+      }).then((result, error) => {
         var options = {
-          requestPermissions: ['identity.basic', 'identity.email']
+          requestPermissions: ["identity.basic", "identity.email"]
         };
-        Meteor.loginWithSlack(options);
+
+        if (result.value) {
+          Meteor.loginWithSlack(options);
+        } else if (
+          result.dismiss === "cancel" ||
+          result.dismiss === "esc" ||
+          result.dismiss === "overlay"
+        ) {
+          swal("Okay", "You can sign in with slack at anytime.", "info");
+        } else {
+          swal(
+            "Oops! Something went wrong",
+            error.error,
+            +"\n Try again",
+            "error"
+          );
+        }
       });
     }
-
   }
 });
