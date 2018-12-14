be588a3e96766f57f65354170f586177791943f2
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2018-12-14 13:42:14 +0530

    Add hash tag support for jarvis

---
+++ server/slack/commands.js
@@ -0,0 +1,10 @@
+// All commands are case insensitive
+const Commands = {
+  TIL: ["#til", "til", "#todayilearn"],
+  HELP: ["#help"],
+  CREATE_HANGOUT: ["#createhangout"],
+  LIST_HANGOUTS: ["#listhangouts", "#listhangout"],
+  HOW_ARE_YOU: ["#howareyou", "#howareyou?"]
+};
+
+export default Commands;
