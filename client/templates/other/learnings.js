afc0bcbbbfa6ef9be4b85be48a42985a22a913f4
Linda <linda.peng@alumni.duke.edu>
2018-12-22 23:58:00 -0500

    Added a codebuddies.org/learnings page (#1094)
    * Add event helper which redirects user to conversation with other user if they click on the envelope icon
    
    * Created foundation of /learnings page, which display count and list of global learnings, plus textarea
    
    * Add links to 24/7 silent hangout and learnings to nav and footer

---
+++ client/templates/other/learnings.js
@@ -0,0 +1,79 @@
+Template.learnings.onCreated(function() {
+  var instance = this;
+  // initialize the reactive variables
+  instance.loaded = new ReactiveVar(0);
+  instance.limit = new ReactiveVar(10);
+  instance.totalNumberOfLearnings = new ReactiveVar(0);
+
+  instance.autorun(function() {
+    // get the limit
+    var limit = instance.limit.get();
+
+    //console.log("Asking for "+limit+" learnings…")
+
+    // subscribe to the posts publication
+    var subscription = instance.subscribe("learnings", limit);
+
+    // if subscription is ready, set limit to newLimit
+    if (subscription.ready()) {
+      //console.log("> Received "+limit+" learnings. \n\n")
+      instance.loaded.set(limit);
+    } else {
+      //console.log("> Subscription is not ready yet. \n\n");
+    }
+
+    // get total number of learnings
+    Meteor.call("getTotalNumberOfLearnings", function(error, result) {
+      if (error) {
+        console.log("error", error);
+      }
+      if (result) {
+        instance.totalNumberOfLearnings.set(result);
+      }
+    });
+  });
+  instance.learnings = function() {
+    return Learnings.find({}, { limit: instance.loaded.get(), sort: { created_at: -1 } });
+  };
+});
+Template.learnings.helpers({
+  learnedUsersCount: function() {
+    return Template.instance().totalNumberOfLearnings.get();
+  },
+  learnings: function() {
+    return Template.instance().learnings();
+  },
+  // are there more posts to show?
+  hasMoreLearnings: function() {
+    return (
+      Template.instance()
+        .learnings()
+        .count() >= Template.instance().limit.get()
+    );
+  }
+});
+
+Template.learnings.events({
+  "click #load-more-learnings": function(event, instance) {
+    event.preventDefault();
+
+    // get current value for limit, i.e. how many posts are currently displayed
+    var limit = instance.limit.get();
+
+    // increase limit by 5 and update it
+    limit += 5;
+    instance.limit.set(limit);
+  },
+  "click .signIn": function(event) {
+    var options = {
+      requestPermissions: ["identity.basic", "identity.email"]
+    };
+    Meteor.loginWithSlack(options);
+  },
+  "click .signInGithub": function(event) {
+    var options = {
+      requestPermissions: ["read:user", "user:email"]
+    };
+    Meteor.loginWithGithub(options);
+  }
+});
