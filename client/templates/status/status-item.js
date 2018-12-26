e5e9583323a99131885c55f87c25f9a5b9f6c0d3
Linda <linda.peng@alumni.duke.edu>
2018-12-25 20:38:04 -0500

    1.2.2 Add-Ons (#1095)
    * Added a codebuddies.org/learnings page (#1094)
    
    * Add event helper which redirects user to conversation with other user if they click on the envelope icon
    
    * Created foundation of /learnings page, which display count and list of global learnings, plus textarea
    
    * Add links to 24/7 silent hangout and learnings to nav and footer
    
    * Add event helper which redirects user to conversation with other user if they click on the envelope icon (#1091)

--- client/templates/status/status-item.js
+++ client/templates/status/status-item.js
@@ -1,17 +1,29 @@
 Template.statusItem.helpers({
   hangoutStatus: function() {
-    if (this.statusHangout == 'silent')
-      return "fa-microphone-slash";
-    else if (this.statusHangout == 'teaching')
-      return "fa-user";
-    else if (this.statusHangout == "collaboration")
-      return "fa-users";
-    else
-      return "fa-microphone-slash";
+    if (this.statusHangout == "silent") return "fa-microphone-slash";
+    else if (this.statusHangout == "teaching") return "fa-user";
+    else if (this.statusHangout == "collaboration") return "fa-users";
+    else return "fa-microphone-slash";
   },
   logged_in_using_codebuddies_team: function() {
     if (!(Meteor.loggingIn() || Meteor.userId())) {
       return Meteor.user().profile.team_id === Meteor.settings.team_id;
     }
+  }
+});
+
+Template.statusItem.events({
+  "click .message": function(e) {
+    const data = {
+      userId: e.target.dataset.id
+    };
+    Meteor.call("conversation.getId", data, function(error, result) {
+      if (error) {
+        console.log("error", error);
+      }
+      if (result) {
+        FlowRouter.go(`/conversation/${result}`);
+      }
+    });
   }
 });
