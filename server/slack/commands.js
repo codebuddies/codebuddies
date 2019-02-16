864cf1ed185324ca673364a4976a0a158ba60c31
Linda <linda.peng@alumni.duke.edu>
2019-02-16 11:26:34 -0800

    New release! (#1114)
    * Added a codebuddies.org/learnings page (#1094)
    
    * Add event helper which redirects user to conversation with other user if they click on the envelope icon
    
    * Created foundation of /learnings page, which display count and list of global learnings, plus textarea
    
    * Add links to 24/7 silent hangout and learnings to nav and footer
    
    * Add event helper which redirects user to conversation with other user if they click on the envelope icon (#1091)
    
    * Modify the 'Thank you for sharing something you learned' message to be friendlier (#1096)
    
    * Delete a duplicated div
    
    * Make leave this group button red
    
    * Resolves issue 1099: updated text on /learnings page
    
    * Fix for 1107: Study groups page should be sorted by last activity, at least as an option
    
    * Update learnings.html
    
    * Add iCalFeed support
    
    * Add productid and method to ical
    
    * Resolves issue 1099: updated text on /learnings page
    
    * CSS height issue
    
    * adjust Slack commands for populating /learnings page
    
    * Update Slack auto-response message
    
    * CSS cleanup + remove Connect google form link from nav until it is ready
    
    * update hangout text
    
    * update groups' text
    
    * Add the # of hangouts scheduled on the old hangouts.codebuddies.org site between January 2015 through April 2016 to the global count
    
    * reverse homepage link

--- server/slack/commands.js
+++ server/slack/commands.js
@@ -1,9 +1,9 @@
 // All commands are case insensitive
 const Commands = {
-  TIL: ["#til", "til", "#todayilearn", "/til", "/todayilearned"],
+  TIL: ["#til", "#win", "/win", "#todayilearned", "/til", "/todayilearned"],
   HELP: ["#help"],
   CREATE_HANGOUT: ["#createhangout", "/createhangout"],
-  LIST_HANGOUTS: ["#listhangouts", "#listhangout", "/listhangout"],
+  LIST_HANGOUTS: ["#listhangouts", "#listhangouts", "/listhangouts"],
   HOW_ARE_YOU: ["#howareyou", "#howareyou?", "/howareyou?"]
 };
 
