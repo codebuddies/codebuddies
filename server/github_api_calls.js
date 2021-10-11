b72b1503fdfbc2470a43383794557ec1f438b54a
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2021-10-11 01:11:48 -0700

    Nest the try/catch blocks

--- server/github_api_calls.js
+++ server/github_api_calls.js
@@ -6,28 +6,30 @@ githubAuthRevoke = function(accessToken) {
 
   // Requesting a user's GitHub identity
   try {
-  const response = HTTP.get(
-    "https://github.com/login/oauth/authorize/",
-    {client_id: clientId, redirect_uri: "https://codebuddies.org/_oauth/github"}
-  );
-  console.log('response', response)
+    const response = HTTP.get(
+      "https://github.com/login/oauth/authorize/",
+      {client_id: clientId, redirect_uri: "https://codebuddies.org/_oauth/github"}
+    );
+    console.log('response after requesting a GitHub identity', response)
+    // After users are redirected back to the site by GitHub
+    try {
+      const { code } = response;
+      console.log('code returned', code);
+      const response = HTTP.post(
+        "https://github.com/login/oauth/access_token",
+        {client_id: clientId, client_secret: clientSecret, code}
+      );
+      console.log('response after users are redirected back to the site by GitHub', response)
+    } catch(e) {
+      console.log("After users are redirected back to the site by GitHub", e);
+      return false;
+    }
   } catch(e) {
     console.log(e)
     return false;
   }
 
-  // After users are redirected back to the site by GitHub
-  try {
-    const { code } = response;
-    console.log('code', code);
-    const response = HTTP.post(
-      "https://github.com/login/oauth/access_token",
-      {client_id: clientId, client_secret: clientSecret, code}
-    );
-  } catch(e) {
-    console.log(e);
-    return false;
-  }
+
 
   return {
     ok: statusCode === 204
