25ea8dbd42dc842fb10ef8a5827a39bead45e6ee
Linda <lindapnotes@gmail.com>
2021-10-14 18:08:09 -0700

    [Issue 1171] GitHub login - another try (#1173)
    * Second attempt following instructions at https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
    
    * Nest the try/catch blocks
    
    Co-authored-by: Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>

--- server/github_api_calls.js
+++ server/github_api_calls.js
@@ -1,20 +1,36 @@
 githubAuthRevoke = function(accessToken) {
+  console.log('testing github')
   check(accessToken, String);
   const clientId = Meteor.settings.github_clientid;
   const clientSecret = Meteor.settings.github_clientsecret;
-  const options = {
-    headers: {
-      "User-Agent": "CodeBuddies",
-      "Authorization": `token ${accessToken}`
-    },
-    auth: `${clientId}:${clientSecret}`
-  };
-  const response = HTTP.del(
-    "https://api.github.com/user/repos",
-    options
-  );
-  const { statusCode = null } = response || {};
-  console.log(response) //Needed for debugging
+
+  // Requesting a user's GitHub identity
+  try {
+    const response = HTTP.get(
+      "https://github.com/login/oauth/authorize/",
+      {client_id: clientId, redirect_uri: "https://codebuddies.org/_oauth/github"}
+    );
+    console.log('response after requesting a GitHub identity', response)
+    // After users are redirected back to the site by GitHub
+    try {
+      const { code } = response;
+      console.log('code returned', code);
+      const response = HTTP.post(
+        "https://github.com/login/oauth/access_token",
+        {client_id: clientId, client_secret: clientSecret, code}
+      );
+      console.log('response after users are redirected back to the site by GitHub', response)
+    } catch(e) {
+      console.log("After users are redirected back to the site by GitHub", e);
+      return false;
+    }
+  } catch(e) {
+    console.log(e)
+    return false;
+  }
+
+
+
   return {
     ok: statusCode === 204
   };
